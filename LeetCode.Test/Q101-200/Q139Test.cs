8076e31fa956fec2bdc94fdc422005c957f498c0
Woei Loon <woeiloon96@gmail.com>
2020-12-18 09:35:26 +0800

    Q139 word break

---
+++ LeetCode.Test/Q101-200/Q139Test.cs
@@ -0,0 +1,51 @@
+﻿using LeetCode.Q101_200;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Test.Q101_200
+{
+    public class Q139Test
+    {
+        [Test]
+        public void Test()
+        {
+            bool result = new Q139().WordBreak("catsandog", new List<string> { "cats", "dog", "sand", "and", "cat" });
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            bool result = new Q139().WordBreak("a", new List<string> { "a" });
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            bool result = new Q139().WordBreak("ab", new List<string> { "a", "b" });
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            bool result = new Q139().WordBreak("leetcode", new List<string> { "leet", "code" });
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            bool result = new Q139().WordBreak("catsandog", new List<string> { "cats", "dog", "sand", "and", "cat" });
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void Test6()
+        {
+            bool result = new Q139().WordBreak("aaaaaaa", new List<string> { "aaaa", "aaa" });
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ LeetCode/Q101-200/Q139.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Q101_200
+{
+    public class Q139
+    {
+        public bool WordBreak(string s, IList<string> wordDict)
+        {
+            bool[] dp = new bool[s.Length + 1];
+            dp[0] = true;
+
+            for (int right = 1; right <= s.Length; right++)
+            {
+                for (int left = right - 1; left >= 0; left--)
+                {
+                    if(dp[left] && wordDict.Contains(s.Substring(left, right - left)))
+                    {
+                        dp[right] = true;
+                        break;
+                    }
+                }
+            }
+
+            return dp[s.Length];
+        }
+    }
+}
