7c20feb89235158c45177357d93f51577c1ebebc
Woei Loon <woeiloon96@gmail.com>
2020-12-19 12:05:02 +0800

    Q268 Missing Number

---
+++ LeetCode.Test/Q201-300/Q268Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q201_300;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q201_300
+{
+    public class Q268Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q268().MissingNumber(new[] { 3, 0, 1 });
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ LeetCode/Q201-300/Q268.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Q201_300
+{
+    public class Q268
+    {
+        public int MissingNumber(int[] nums)
+        {
+            /* XOR EG:
+             * a ^ 0 = a;
+             * a ^ a = 0;
+             * a ^ b ^ b = a;
+            */
+
+            int result = nums.Length;
+
+            for(int i = 0; i < nums.Length; i++)
+                result = result ^ i ^ nums[i];
+
+            /* EG:
+             * Input: [3, 0, 1]
+             * result = 3 ^ 0 ^ 3 ^ 1 ^ 0 ^ 2 ^ 1
+             * result = 0 ^ 0 ^ 1 ^ 1 ^ 3 ^ 3 ^ 2
+             * result = 2
+            */
+
+            return result;
+        }
+    }
+}
