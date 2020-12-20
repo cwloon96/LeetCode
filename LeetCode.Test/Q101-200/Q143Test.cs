2b4ad2bd71a5226fb992aef83b85c3ef54f38e78
Woei Loon <woeiloon96@gmail.com>
2020-12-20 22:31:03 +0800

    143. Reorder List

---
+++ LeetCode.Test/Q101-200/Q143Test.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Common.Models;
+using LeetCode.Q101_200;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q101_200
+{
+    public class Q143Test
+    {
+        [Test]
+        public void Test()
+        {
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
+
+            new Q143().ReorderList(head);
+
+            Assert.AreEqual(1, head.val);
+            Assert.AreEqual(4, head.next.val);
+            Assert.AreEqual(2, head.next.next.val);
+            Assert.AreEqual(3, head.next.next.next.val);
+        }
+    }
+}
---
+++ LeetCode/Q101-200/Q143.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Common.Models;
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Q101_200
+{
+    public class Q143
+    {
+        public void ReorderList(ListNode head)
+        {
+            Stack<ListNode> stack = new Stack<ListNode>();
+            ListNode copy = head;
+            while (copy != null)
+            {
+                stack.Push(copy);
+                copy = copy.next;
+            }
+
+            int size = stack.Count;
+
+            while (stack.Count > Math.Ceiling((double)size / 2))
+            {
+                ListNode temp = head.next;
+                head.next = stack.Pop();
+                head.next.next = temp;
+                head = head.next.next;
+            }
+
+            if (head != null)
+                head.next = null;
+        }
+    }
+}
