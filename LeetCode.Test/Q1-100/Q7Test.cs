1b48c01e78cd78e0660db8317c4941b5d54eda14
Woei Loon <woeiloon96@gmail.com>
2020-12-21 07:13:19 +0800

    7. Reverse Integer

---
+++ LeetCode.Test/Q1-100/Q7Test.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Q1_100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1_100
+{
+    public class Q7Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q7().Reverse(123);
+
+            Assert.AreEqual(321, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int result = new Q7().Reverse(1534236469);
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q7.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Q1_100
+{
+    public class Q7
+    {
+        public int Reverse(int x)
+        {
+            int result = 0;
+
+            while (x != 0)
+            {
+                // result * 10 to add one more digit
+                // x % 10 to get last digit in x
+                int newResult = (result * 10) + (x % 10);
+
+                // newResult / 10 equals newResult after removing last digit
+                // check if newResult is still similar with previos result after removing last digit
+                if (newResult / 10 != result)
+                    return 0;
+
+                x /= 10;
+                result = newResult;
+            }
+
+            return result;
+        }
+    }
+}
