72f22d0881b613c94287ffe155f931f8776a468f
Woei Loon <woeiloon96@gmail.com>
2020-12-22 08:18:19 +0800

    605. Can Place Flowers

---
+++ LeetCode.Test/Q601-700/Q605Test.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode.Q601_700;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Test.Q601_700
+{
+    public class Q605Test
+    {
+        [Test]
+        public void Test()
+        {
+            bool result = new Q605().CanPlaceFlowers(new[] { 0 }, 1);
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            bool result = new Q605().CanPlaceFlowers(new[] { 1, 0, 0, 0, 1 }, 2);
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void TestNoMutate()
+        {
+            bool result = new Q605().CanPlaceFlowersNoMutate(new[] { 0 }, 1);
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void TestNoMutate2()
+        {
+            bool result = new Q605().CanPlaceFlowersNoMutate(new[] { 1, 0, 0, 0, 1 }, 2);
+            Assert.AreEqual(false, result);
+        }
+
+    }
+}
---
+++ LeetCode/Q601-700/Q605.cs
@@ -0,0 +1,55 @@
+﻿namespace LeetCode.Q601_700
+{
+    public class Q605
+    {
+        public bool CanPlaceFlowers(int[] flowerbed, int n)
+        {
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if (n == 0)
+                    break;
+
+                if (flowerbed[i] == 0)
+                {
+                    if (i > 0 && flowerbed[i - 1] == 1)
+                        continue;
+
+                    if (i < flowerbed.Length - 1 && flowerbed[i + 1] == 1)
+                        continue;
+
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+
+            return n == 0;
+        }
+
+        public bool CanPlaceFlowersNoMutate(int[] flowerbed, int n)
+        {
+            int currentEmpty = 0;
+            for (int i = 0; i <= flowerbed.Length; i++)
+            {
+                // i < flowerbed.Length force it go to else statement after reaching last element
+                if (i < flowerbed.Length && flowerbed[i] == 0)
+                {
+                    currentEmpty++;
+
+                    // separate to two if statement to fit the case [0]
+                    // to represent left of first element or right of last element
+                    if (i == 0) currentEmpty++;
+                    if (i == flowerbed.Length - 1) currentEmpty++;
+                }
+                else
+                {
+                    // if 3 zero in a row, only 1 slot can be placed
+                    n -= (currentEmpty - 1) / 2;
+                    if (n <= 0) return true;
+                    currentEmpty = 0;
+                }
+            }
+
+            return false;
+        }
+    }
+}
