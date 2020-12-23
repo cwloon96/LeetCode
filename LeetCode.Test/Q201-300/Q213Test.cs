e24179ed21c3371f606dd28e36d7bb28b0ee457a
Woei Loon <woeiloon96@gmail.com>
2020-12-24 06:47:12 +0800

    213. House Robber II

---
+++ LeetCode.Test/Q201-300/Q213Test.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Q201_300;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q201_300
+{
+    public class Q213Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q213().Rob(new[] { 1, 2, 3, 1 });
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int result = new Q213().Rob(new[] { 1 });
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ LeetCode/Q201-300/Q213.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode.Q201_300
+{
+    public class Q213
+    {
+        public int Rob(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return nums[0];
+
+            return Math.Max(Rob(nums, 0, nums.Length - 1), Rob(nums, 1, nums.Length));
+        }
+
+        private int Rob(int[] nums, int left, int right)
+        {
+            int prevOne = 0;
+            int prevTwo = 0;
+            int max = 0;
+
+            for (int i = left; i < right; i++)
+            {
+                max = Math.Max(prevTwo + nums[i], prevOne);
+                prevTwo = prevOne;
+                prevOne = max;
+            }
+
+            return max;
+        }
+    }
+}
