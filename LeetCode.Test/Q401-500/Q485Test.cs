9430899968d5444692076ae856f6f354a92e9735
Woei Loon <woeiloon96@gmail.com>
2020-12-25 08:45:14 +0800

    485. Max Consecutive Ones

---
+++ LeetCode.Test/Q401-500/Q485Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q401_500;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q401_500
+{
+    public class Q485Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q485().FindMaxConsecutiveOnes(new[] { 1, 1, 0, 1, 1, 1 });
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ LeetCode/Q401-500/Q485.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Q401_500
+{
+    public class Q485
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            int count = 0;
+            int max = 0;
+            foreach (int num in nums)
+            {
+                if (num == 1)
+                {
+                    count++;
+                    max = max > count ? max : count;
+                }
+                else count = 0;
+            }
+            return max;
+        }
+    }
+}
