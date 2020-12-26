181f49becbc50cc76f4f5170db51dff40d9bc3f6
Woei Loon <woeiloon96@gmail.com>
2020-12-26 12:45:22 +0800

    337. House Robber III

---
+++ LeetCode.Test/Q301-400/Q337Test.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Common.Models;
+using LeetCode.Q301_400;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q301_400
+{
+    public class Q337Test
+    {
+        [Test]
+        public void Test()
+        {
+            TreeNode root = new TreeNode(3);
+            root.left = new TreeNode(2);
+            root.left.right = new TreeNode(3);
+
+            root.right = new TreeNode(3);
+            root.right.right = new TreeNode(1);
+
+            int result = new Q337().Rob(root);
+            Assert.AreEqual(7, result);
+        }
+    }
+}
---
+++ LeetCode/Q301-400/Q337.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Common.Models;
+using System;
+
+namespace LeetCode.Q301_400
+{
+    public class Q337
+    {
+        public int Rob(TreeNode root)
+        {
+            int[] result = RobSub(root);
+            return Math.Max(result[0], result[1]);
+        }
+
+        private int[] RobSub(TreeNode root)
+        {
+            if (root == null) return new int[2];
+
+            int[] left = RobSub(root.left);
+            int[] right = RobSub(root.right);
+
+            // 0 = no rob, 1 = rob
+            int[] res = new int[2];
+
+            // no rob root, so can get the largest from left / right
+            res[0] = Math.Max(left[0], left[1]) + Math.Max(right[0], right[1]);
+
+            // rob root, thus get element 0 from left & right which is no rob
+            res[1] = root.val + left[0] + right[0];
+
+            return res;
+        }
+    }
+}
