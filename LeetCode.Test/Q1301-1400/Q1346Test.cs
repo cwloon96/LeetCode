97b09bacae5829a32780747a9a40aca25317fd37
Woei Loon <woeiloon96@gmail.com>
2020-12-28 07:27:27 +0800

    1346. Check If N and Its Double Exist

---
+++ LeetCode.Test/Q1301-1400/Q1346Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q1301_1400;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1301_1400
+{
+    public class Q1346Test
+    {
+        [Test]
+        public void Test()
+        {
+            bool result = new Q1346().CheckIfExist(new[] { 10, 2, 5, 3 });
+
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ LeetCode/Q1301-1400/Q1346.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Q1301_1400
+{
+    public class Q1346
+    {
+        public bool CheckIfExist(int[] arr)
+        {
+            HashSet<double> hashset = new HashSet<double>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (hashset.Contains(arr[i] * 2) || hashset.Contains((double)arr[i] / 2))
+                    return true;
+
+                hashset.Add(arr[i]);
+            }
+
+            return false;
+        }
+    }
+}
