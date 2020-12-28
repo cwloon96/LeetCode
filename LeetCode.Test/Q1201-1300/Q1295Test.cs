7b75907d8fc5314e891c91d9cac61af5759e46a7
Woei Loon <woeiloon96@gmail.com>
2020-12-29 06:51:23 +0800

    1295. Find Numbers with Even Number of Digits

---
+++ LeetCode.Test/Q1201-1300/Q1295Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q1201_1300;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1201_1300
+{
+    class Q1295Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q1295().FindNumbers(new[] { 555, 901, 482, 1771 });
+
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ LeetCode/Q1201-1300/Q1295.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace LeetCode.Q1201_1300
+{
+    public class Q1295
+    {
+        public int FindNumbers(int[] nums)
+        {
+            return nums.Count(x => x.ToString().Length % 2 == 0);
+        }
+    }
+}
