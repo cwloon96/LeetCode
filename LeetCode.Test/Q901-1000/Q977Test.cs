b0dd5555fd2382238893e80ff40a57cfc44b0218
Woei Loon <woeiloon96@gmail.com>
2020-12-29 06:49:09 +0800

    977. Squares of a Sorted Array

---
+++ LeetCode.Test/Q901-1000/Q977Test.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Q901_1000;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q901_1000
+{
+    public class Q977Test
+    {
+        [Test]
+        public void Test()
+        {
+            int[] result = new Q977().SortedSquares(new[] { -4, -1, 0, 3, 10 });
+
+            Assert.AreEqual(result[0], 0);
+            Assert.AreEqual(result[1], 1);
+            Assert.AreEqual(result[2], 9);
+            Assert.AreEqual(result[3], 16);
+            Assert.AreEqual(result[4], 100);
+        }
+    }
+}
---
+++ LeetCode/Q901-1000/Q977.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Q901_1000
+{
+    public class Q977
+    {
+        public int[] SortedSquares(int[] A)
+        {
+            int begin = 0;
+            int end = A.Length - 1;
+            int[] ans = new int[A.Length];
+            int ansIndex = A.Length - 1;
+
+            while (begin <= end)
+            {
+                int pow1 = A[begin] * A[begin];
+                int pow2 = A[end] * A[end];
+
+                if (pow1 < pow2)
+                {
+                    ans[ansIndex--] = pow2;
+                    end--;
+                }
+                else
+                {
+                    ans[ansIndex--] = pow1;
+                    begin++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
