7cd5c9474a98ff88312d8c1cf80dc642d8a622d9
Woei Loon <woeiloon96@gmail.com>
2020-12-29 07:39:36 +0800

    fix wrong test case for Q977

--- LeetCode.Test/Q901-1000/Q977Test.cs
+++ LeetCode.Test/Q901-1000/Q977Test.cs
@@ -10,11 +10,11 @@ namespace LeetCode.Test.Q901_1000
         {
             int[] result = new Q977().SortedSquares(new[] { -4, -1, 0, 3, 10 });
 
-            Assert.AreEqual(result[0], 0);
-            Assert.AreEqual(result[1], 1);
-            Assert.AreEqual(result[2], 9);
-            Assert.AreEqual(result[3], 16);
-            Assert.AreEqual(result[4], 100);
+            Assert.AreEqual(0, result[0]);
+            Assert.AreEqual(1, result[1]);
+            Assert.AreEqual(9, result[2]);
+            Assert.AreEqual(16, result[3]);
+            Assert.AreEqual(100, result[4]);
         }
     }
 }
