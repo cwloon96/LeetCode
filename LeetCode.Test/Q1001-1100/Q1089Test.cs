2bb1aff9ec534866ed0972a98cceda94841f8447
Woei Loon <woeiloon96@gmail.com>
2020-12-29 08:18:37 +0800

    1089. Duplicate Zeros

---
+++ LeetCode.Test/Q1001-1100/Q1089Test.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Q1001_1100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1001_1100
+{
+    public class Q1089Test
+    {
+        [Test]
+        public void Test()
+        {
+            int[] arr = new[] { 1, 0, 2, 3, 0, 4, 5, 0 };
+            new Q1089().DuplicateZeros(arr);
+
+            Assert.AreEqual(1, arr[0]);
+            Assert.AreEqual(0, arr[1]);
+            Assert.AreEqual(0, arr[2]);
+            Assert.AreEqual(2, arr[3]);
+            Assert.AreEqual(3, arr[4]);
+            Assert.AreEqual(0, arr[5]);
+            Assert.AreEqual(0, arr[6]);
+            Assert.AreEqual(4, arr[7]);
+        }
+    }
+}
---
+++ LeetCode/Q1001-1100/Q1089.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Q1001_1100
+{
+    public class Q1089
+    {
+        public void DuplicateZeros(int[] arr)
+        {
+            int zeroCnt = 0;
+            foreach (int num in arr)
+            {
+                if (num == 0)
+                    zeroCnt++;
+            }
+
+            // left & right keep decreasing to remove the elements which not included in result array
+            for (int left = arr.Length - 1, right = arr.Length + zeroCnt - 1; 
+                left >= 0 && left < right; 
+                left--, right--)
+            {
+                // right is within array length, which mean the unused element already skipped in left, assign right = left
+                if (right < arr.Length)
+                    arr[right] = arr[left];
+
+                if (arr[left] == 0)
+                {
+                    // since assign from right, which mean the duplicated 0 need to appear in front of right, so right--
+                    right--;
+
+                    if (right < arr.Length)
+                        arr[right] = 0;
+                }
+            }
+        }
+    }
+}
