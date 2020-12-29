be23184bc6012efaf982e375297be169acfb3684
Woei Loon <woeiloon96@gmail.com>
2020-12-30 06:57:10 +0800

    88. Merge Sorted Array

---
+++ LeetCode.Test/Q1-100/Q88Test.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Q1_100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1_100
+{
+    public class Q88Test
+    {
+        [Test]
+        public void Test()
+        {
+            int[] nums1 = new[] { 1, 2, 3, 0, 0, 0 };
+            int[] nums2 = new[] { 2, 5, 6 };
+
+            new Q88().Merge(nums1, 3, nums2, 3);
+
+            Assert.AreEqual(1, nums1[0]);
+            Assert.AreEqual(2, nums1[1]);
+            Assert.AreEqual(2, nums1[2]);
+            Assert.AreEqual(3, nums1[3]);
+            Assert.AreEqual(5, nums1[4]);
+            Assert.AreEqual(6, nums1[5]);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q88.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Q1_100
+{
+    public class Q88
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            for (int i = m + n - 1, j = m - 1, k = n - 1; i >= 0; i--)
+            {
+                if (j >= 0 && (k < 0 || nums1[j] > nums2[k]))
+                    nums1[i] = nums1[j--];
+                else
+                    nums1[i] = nums2[k--];
+            }
+        }
+    }
+}
