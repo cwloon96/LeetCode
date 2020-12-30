858ada36211c81c4704b08e48f6666ce2f5df0a2
Woei Loon <woeiloon96@gmail.com>
2020-12-31 06:30:09 +0800

    27. Remove Element

---
+++ LeetCode.Test/Q1-100/Q27Test.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Q1_100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1_100
+{
+    public class Q27Test
+    {
+        [Test]
+        public void Test()
+        {
+            int[] arr = new[] { 3, 2, 2, 3 };
+            int result = new Q27().RemoveElement(arr, 3);
+
+            Assert.AreEqual(2, result);
+            Assert.AreEqual(2, arr[0]);
+            Assert.AreEqual(2, arr[1]);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int[] arr = new[] { 1 };
+            int result = new Q27().RemoveElement(arr, 1);
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q27.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Q1_100
+{
+    public class Q27
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                if (nums[left] == val)
+                {
+                    nums[left] = nums[right];
+                    right--;
+                }
+                else
+                    left++;
+            }
+
+            return left;
+        }
+    }
+}
