56135f65f56a287ad7b54ed6a908ae6276056f06
Woei Loon <woeiloon96@gmail.com>
2021-01-03 07:15:14 +0800

    1299. Replace Elements with Greatest Element on Right Side

---
+++ LeetCode.Test/Q1201-1300/Q1299Test.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Q1201_1300;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1201_1300
+{
+    class Q1299Test
+    {
+        [Test]
+        public void Test()
+        {
+            int[] result = new Q1299().ReplaceElements(new[] { 17, 18, 5, 4, 6, 1 });
+
+            Assert.AreEqual(18, result[0]);
+            Assert.AreEqual(6, result[1]);
+            Assert.AreEqual(6, result[2]);
+            Assert.AreEqual(6, result[3]);
+            Assert.AreEqual(1, result[4]);
+            Assert.AreEqual(-1, result[5]);
+        }
+    }
+}
---
+++ LeetCode/Q1201-1300/Q1299.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCode.Q1201_1300
+{
+    public class Q1299
+    {
+        public int[] ReplaceElements(int[] arr)
+        {
+            int max = -1;
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                int temp = arr[i];
+                arr[i] = max;
+                max = Math.Max(temp, max);
+            }
+
+            return arr;
+        }
+    }
+}
