f8ccdc550c80d72256209e07688bc20b18c7daf0
Woei Loon <woeiloon96@gmail.com>
2021-01-02 08:28:07 +0800

    941. Valid Mountain Array

---
+++ LeetCode.Test/Q901-1000/Q941Test.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Q901_1000;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q901_1000
+{
+    public class Q941Test
+    {
+        [Test]
+        public void Test()
+        {
+            bool result = new Q941().ValidMountainArray(new[] { 2, 1 });
+
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            bool result = new Q941().ValidMountainArray(new[] { 3, 5, 5 });
+
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            bool result = new Q941().ValidMountainArray(new[] { 0, 3, 2, 1 });
+
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ LeetCode/Q901-1000/Q941.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Q901_1000
+{
+    public class Q941
+    {
+        public bool ValidMountainArray(int[] arr)
+        {
+            if (arr.Length == 1)
+                return false;
+
+            int i = 0;
+            while (i < arr.Length - 1 && arr[i] < arr[i + 1])
+                i++;
+
+            if (i == 0 || i == arr.Length - 1)
+                return false;
+
+            while (i < arr.Length - 1 && arr[i] > arr[i + 1])
+                i++;
+
+            return i == arr.Length - 1;
+        }
+    }
+}
