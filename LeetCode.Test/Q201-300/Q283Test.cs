2436374c9116ceafab371a9c6dd902ca16e0792c
Woei Loon <woeiloon96@gmail.com>
2021-01-04 06:54:02 +0800

    283. Move Zeroes

---
+++ LeetCode.Test/Q201-300/Q283Test.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Q201_300;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q201_300
+{
+    class Q283Test
+    {
+        [Test]
+        public void Test()
+        {
+            int[] arr = new[] { 0, 1, 0, 3, 12 };
+            new Q283().MoveZeroes(arr);
+
+            Assert.AreEqual(1, arr[0]);
+            Assert.AreEqual(3, arr[1]);
+            Assert.AreEqual(12, arr[2]);
+            Assert.AreEqual(0, arr[3]);
+            Assert.AreEqual(0, arr[4]);
+        }
+    }
+}
---
+++ LeetCode/Q201-300/Q283.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Q201_300
+{
+    public class Q283
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            for (int left = 0, right = 1; right < nums.Length; right++)
+            {
+                if (nums[left] == 0 && nums[right] != 0)
+                {
+                    nums[left] = nums[right];
+                    nums[right] = 0;
+                    left++;
+                }
+
+                if (nums[left] != 0)
+                    left++;
+            }
+        }
+    }
+}
