55da21cc95f830b4bf1d2183b06aeba7b29dee17
Woei Loon <woeiloon96@gmail.com>
2021-01-05 06:56:49 +0800

    905. Sort Array By Parity

---
+++ LeetCode.Test/Q901-1000/Q905Test.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Q901_1000;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q901_1000
+{
+    class Q905Test
+    {
+        [Test]
+        public void Test()
+        {
+            int[] result = new Q905().SortArrayByParity(new[] { 3, 1, 2, 4 });
+
+            Assert.AreEqual(2, result[0]);
+            Assert.AreEqual(4, result[1]);
+            Assert.AreEqual(3, result[2]);
+            Assert.AreEqual(1, result[3]);
+        }
+    }
+}
---
+++ LeetCode/Q901-1000/Q905.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Q901_1000
+{
+    public class Q905
+    {
+        public int[] SortArrayByParity(int[] A)
+        {
+            int left = 0;
+            int right = 1;
+
+            while (right < A.Length)
+            {
+                if (A[left] % 2 != 0 && A[right] % 2 == 0)
+                {
+                    int temp = A[right];
+                    A[right] = A[left];
+                    A[left] = temp;
+                }
+
+                if (A[left] % 2 == 0)
+                    left++;
+
+                right++;
+            }
+
+            return A;
+        }
+    }
+}
