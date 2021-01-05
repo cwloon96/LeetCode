e09d3922f3083a9bb42f2778e08b1297457fe512
Woei Loon <woeiloon96@gmail.com>
2021-01-06 07:16:03 +0800

    1051. Height Checker

---
+++ LeetCode.Test/Q1001-1100/Q1051Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q1001_1100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1001_1100
+{
+    class Q1051Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q1051().HeightChecker(new[] { 1, 1, 4, 2, 1, 3 });
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ LeetCode/Q1001-1100/Q1051.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Q1001_1100
+{
+    public class Q1051
+    {
+        public int HeightChecker(int[] heights)
+        {
+            int[] arr = new int[101];
+
+            for (int i = 0; i < heights.Length; i++)
+                arr[heights[i]]++;
+
+            int currentHeight = 0;
+            int result = 0;
+
+            for (int i = 0; i < heights.Length; i++)
+            {
+                while (arr[currentHeight] == 0)
+                    currentHeight++;
+
+                if (heights[i] != currentHeight)
+                    result++;
+
+                arr[currentHeight]--;
+            }
+
+            return result;
+        }
+    }
+}
