a5291da51c1dd13d2e44b271e080d97603d54b78
Woei Loon <woeiloon96@gmail.com>
2021-01-07 07:02:25 +0800

    414. Third Maximum Number

---
+++ LeetCode.Test/Q401-500/Q414Test.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Q401_500;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q401_500
+{
+    class Q414Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q414().ThirdMax(new[] { 2, 1 });
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int result = new Q414().ThirdMax(new[] { 3, 2, 1 });
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ LeetCode/Q401-500/Q414.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Q401_500
+{
+    public class Q414
+    {
+        public int ThirdMax(int[] nums)
+        {
+            SortedSet<int> sortedSet = new SortedSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sortedSet.Add(nums[i]);
+                if (sortedSet.Count() > 3)
+                    sortedSet.Remove(sortedSet.Min);
+            }
+
+            return sortedSet.Count() == 3 ? sortedSet.Min : sortedSet.Max;
+        }
+    }
+}
