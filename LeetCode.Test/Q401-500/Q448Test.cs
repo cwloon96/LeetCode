0d058a41c8fbd197c81b67e59f375afd7337f160
Woei Loon <woeiloon96@gmail.com>
2021-01-08 06:57:28 +0800

    448. Find All Numbers Disappeared in an Array

---
+++ LeetCode.Test/Q401-500/Q448Test.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Q401_500;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Test.Q401_500
+{
+    class Q448Test
+    {
+        [Test]
+        public void Test()
+        {
+            IList<int> result = new Q448().FindDisappearedNumbers(new[] { 4, 3, 2, 7, 8, 2, 3, 1 });
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(true, result.Contains(5));
+            Assert.AreEqual(true, result.Contains(6));
+        }
+    }
+}
---
+++ LeetCode/Q401-500/Q448.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Q401_500
+{
+    public class Q448
+    {
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                while (nums[nums[i] - 1] != nums[i])
+                {
+                    // temp cannot be nums[i], else when we update nums[i] in second row, the third row will be affected
+                    int temp = nums[nums[i] - 1];
+                    nums[nums[i] - 1] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+
+            List<int> result = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+                if (nums[i] != i + 1)
+                    result.Add(i + 1);
+
+            return result;
+        }
+    }
+}
