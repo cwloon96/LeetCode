578884a10fafe2ee5e90d839009120dda6f0f55c
Woei Loon <woeiloon96@gmail.com>
2021-01-10 07:27:28 +0800

    665. Non-decreasing Array

---
+++ LeetCode.Test/Q601-700/Q665Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q601_700;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q601_700
+{
+    class Q665Test
+    {
+        [Test]
+        public void Test()
+        {
+            bool result = new Q665().CheckPossibility(new[] { 4, 2, 3 });
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ LeetCode/Q601-700/Q665.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Q601_700
+{
+    public class Q665
+    {
+        public bool CheckPossibility(int[] nums)
+        {
+            bool found = false;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] < nums[i - 1])
+                {
+                    if (found)
+                        return false;
+
+                    // eg: [1,2,3,1,2]
+                    if (i >= 2 && nums[i] < nums[i - 2] &&
+                       i + 1 < nums.Length && nums[i - 1] > nums[i + 1])
+                        return false;
+
+                    found = true;
+                }
+            }
+
+            return true;
+        }
+    }
+}
