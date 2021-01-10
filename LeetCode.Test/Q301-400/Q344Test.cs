b28ec51e549012c19f6c3fb8c282d45fd700ceea
Woei Loon <woeiloon96@gmail.com>
2021-01-11 07:10:32 +0800

    344. Reverse String

---
+++ LeetCode.Test/Q301-400/Q344Test.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Q301_400;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q301_400
+{
+    class Q344Test
+    {
+        [Test]
+        public void Test()
+        {
+            char[] arr = new[] {'h', 'e', 'l', 'l', 'o' };
+            new Q344().ReverseString(arr);
+
+            Assert.AreEqual('o', arr[0]);
+            Assert.AreEqual('l', arr[1]);
+            Assert.AreEqual('l', arr[2]);
+            Assert.AreEqual('e', arr[3]);
+            Assert.AreEqual('h', arr[4]);
+        }
+    }
+}
---
+++ LeetCode/Q301-400/Q344.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Q301_400
+{
+    public class Q344
+    {
+        public void ReverseString(char[] s)
+        {
+            for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+            {
+                char temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
+}
