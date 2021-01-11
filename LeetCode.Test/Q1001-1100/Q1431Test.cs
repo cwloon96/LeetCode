a5bfced1533bc2fe34ea7d3de08509327232cea2
Woei Loon <woeiloon96@gmail.com>
2021-01-12 06:30:46 +0800

    1431. Kids With the Greatest Number of Candies

---
+++ LeetCode.Test/Q1001-1100/Q1431Test.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Q1001_1100;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Test.Q1001_1100
+{
+    class Q1431Test
+    {
+        [Test]
+        public void Test()
+        {
+            IList<bool> result = new Q1431().KidsWithCandies(new[] { 2, 3, 5, 1, 3 }, 3);
+
+            Assert.AreEqual(true, result[0]);
+            Assert.AreEqual(true, result[1]);
+            Assert.AreEqual(true, result[2]);
+            Assert.AreEqual(false, result[3]);
+            Assert.AreEqual(true, result[4]);
+        }
+    }
+}
---
+++ LeetCode/Q1001-1100/Q1431.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Q1001_1100
+{
+    public class Q1431
+    {
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            int maxCandies = candies[0];
+            for (int i = 1; i < candies.Length; i++)
+                maxCandies = Math.Max(candies[i], maxCandies);
+
+            List<bool> result = new List<bool>();
+
+            for (int i = 0; i < candies.Length; i++)
+                result.Add(candies[i] + extraCandies >= maxCandies);
+
+            return result;
+        }
+    }
+}
