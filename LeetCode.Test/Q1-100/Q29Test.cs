637984c57b8724cc56c22861e955bffb7200a944
Woei Loon <woeiloon96@gmail.com>
2021-01-13 07:25:25 +0800

    29. Divide Two Integers

---
+++ LeetCode.Test/Q1-100/Q29Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q1_100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1_100
+{
+    class Q29Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q29().Divide(10, 3);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q29.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode.Q1_100
+{
+    public class Q29
+    {
+        public int Divide(int dividend, int divisor)
+        {
+            if (dividend == int.MinValue && divisor == -1)
+                return int.MaxValue;
+            if (dividend == int.MaxValue && divisor == -1)
+                return int.MinValue;
+
+            int result = Calculate(Math.Abs((long)dividend), Math.Abs((long)divisor));
+
+            return (dividend >= 0 == divisor >= 0) ? result : -result;
+        }
+
+        private int Calculate(long dividend, long divisor)
+        {
+            if (divisor > dividend)
+                return 0;
+
+            int count = 1;
+            long accumulate = divisor;
+
+            while (accumulate << 1 <= dividend)
+            {
+                count <<= 1;
+                accumulate <<= 1;
+            }
+
+            return count + Calculate(dividend - accumulate, divisor);
+        }
+    }
+}
