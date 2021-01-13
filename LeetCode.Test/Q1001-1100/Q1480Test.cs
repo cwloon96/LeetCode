588b885aaa7bee6f1d0c1dde583aa92f026b9c63
Woei Loon <woeiloon96@gmail.com>
2021-01-14 06:33:38 +0800

    1480. Running Sum of 1d Array

---
+++ LeetCode.Test/Q1001-1100/Q1480Test.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Q1001_1100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1001_1100
+{
+    class Q1480Test
+    {
+        [Test]
+        public void Test()
+        {
+            int[] result = new Q1480().RunningSum(new[] { 1, 2, 3, 4 });
+
+            Assert.AreEqual(1, result[0]);
+            Assert.AreEqual(3, result[1]);
+            Assert.AreEqual(6, result[2]);
+            Assert.AreEqual(10, result[3]);
+        }
+    }
+}
---
+++ LeetCode/Q1001-1100/Q1480.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Q1001_1100
+{
+    public class Q1480
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+                nums[i] += nums[i - 1];
+
+            return nums;
+        }
+    }
+}
