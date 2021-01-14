d968cffb864c03e7ce75aa25a18eef7014117214
Woei Loon <woeiloon96@gmail.com>
2021-01-15 06:57:03 +0800

    46. Permutations

---
+++ LeetCode.Test/Q1-100/Q46Test.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Q1_100;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Test.Q1_100
+{
+    class Q46Test
+    {
+        [Test]
+        public void Test()
+        {
+            IList<IList<int>> result = new Q46().Permute(new[] { 1, 2, 3 });
+
+            Assert.AreEqual(new List<IList<int>>
+            {
+                new List<int> { 1, 2, 3 },
+                new List<int> { 1, 3, 2 },
+                new List<int> { 2, 1, 3 },
+                new List<int> { 2, 3, 1 },
+                new List<int> { 3, 1, 2 },
+                new List<int> { 3, 2, 1 },
+            }, result);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q46.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Q1_100
+{
+    public class Q46
+    {
+        public IList<IList<int>> Permute(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            BackTracking(nums, new List<int>(), result);
+            return result;
+        }
+
+        private void BackTracking(int[] nums, IList<int> temp, IList<IList<int>> result)
+        {
+            if (temp.Count == nums.Length)
+            {
+                result.Add(new List<int>(temp));
+            }
+            else
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (!temp.Contains(nums[i]))
+                    {
+                        temp.Add(nums[i]);
+                        BackTracking(nums, temp, result);
+                        temp.RemoveAt(temp.Count - 1);
+                    }
+                }
+            }
+        }
+    }
+}
