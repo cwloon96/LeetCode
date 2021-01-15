cfafa22d8332027e7d8b04cf6c256365967bdbae
Woei Loon <woeiloon96@gmail.com>
2021-01-16 07:00:08 +0800

    2. Add Two Numbers

---
+++ LeetCode.Test/Q1-100/Q2Test.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Common.Models;
+using LeetCode.Q1_100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1_100
+{
+    class Q2Test
+    {
+        [Test]
+        public void Test()
+        {
+            ListNode l1 = new ListNode(2);
+            l1.next = new ListNode(4);
+            l1.next.next = new ListNode(3);
+
+            ListNode l2 = new ListNode(5);
+            l2.next = new ListNode(6);
+            l2.next.next = new ListNode(4);
+
+            ListNode result = new Q2().AddTwoNumbers(l1, l2);
+
+            Assert.AreEqual(7, result.val);
+            Assert.AreEqual(0, result.next.val);
+            Assert.AreEqual(8, result.next.next.val);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q2.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Common.Models;
+
+namespace LeetCode.Q1_100
+{
+    public class Q2
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            return Add(l1, l2, 0);
+        }
+
+
+        private ListNode Add(ListNode l1, ListNode l2, int carry)
+        {
+            if (l1 == null && l2 == null && carry == 0)
+                return null;
+
+            int sum = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+            return new ListNode(sum % 10, Add(l1?.next, l2?.next, sum / 10));
+        }
+    }
+}
