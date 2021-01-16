1a61b15d2fd11161178be2edd377ba07bb78c113
Woei Loon <woeiloon96@gmail.com>
2021-01-17 07:08:51 +0800

    78. Subsets

---
+++ LeetCode.Test/Q1-100/Q78Test.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Q1_100;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Test.Q1_100
+{
+    class Q78Test
+    {
+        [Test]
+        public void Test()
+        {
+            IList<IList<int>> result = new Q78().Subsets(new[] { 1, 2, 3 });
+
+            IList<IList<int>> expected = new List<IList<int>>
+            {
+                new List<int> { },
+                new List<int> { 1 },
+                new List<int> { 2 },
+                new List<int> { 1, 2 },
+                new List<int> { 3 },
+                new List<int> { 1, 3 },
+                new List<int> { 2, 3 },
+                new List<int> { 1, 2, 3 },
+            };
+
+            CollectionAssert.AreEquivalent(expected, result);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q78.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Q1_100
+{
+    public class Q78
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            BackTrack(nums, result, new List<int>(), 0);
+            return result;
+        }
+
+        private void BackTrack(int[] nums, IList<IList<int>> result, List<int> temp, int start)
+        {
+            result.Add(new List<int>(temp));
+            for (int i = start; i < nums.Length; i++)
+            {
+                temp.Add(nums[i]);
+                BackTrack(nums, result, temp, i + 1);
+                temp.RemoveAt(temp.Count - 1);
+            }
+        }
+    }
+}
