110bd30fd78f007b2b0bde4056852d38c1425992
Woei Loon <woeiloon96@gmail.com>
2021-01-18 06:30:20 +0800

    39. Combination Sum

---
+++ LeetCode.Test/Q1-100/Q39Test.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Q1_100;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Test.Q1_100
+{
+    class Q39Test
+    {
+        [Test]
+        public void Test()
+        {
+            IList<IList<int>> result = new Q39().CombinationSum(new[] { 2, 3, 5 }, 8);
+
+            IList<IList<int>> expected = new List<IList<int>>
+            {
+                new List<int> { 2, 2, 2, 2 },
+                new List<int> { 2, 3, 3 },
+                new List<int> { 3, 5 },
+            };
+
+            CollectionAssert.AreEquivalent(expected, result);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q39.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Q1_100
+{
+    public class Q39
+    {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            BackTrack(candidates, target, result, new List<int>(), 0);
+            return result;
+        }
+
+        private void BackTrack(int[] candidates, int target, IList<IList<int>> result, List<int> temp, int start)
+        {
+            int sum = temp.Sum();
+
+            if (sum == target)
+                result.Add(new List<int>(temp));
+            else if (sum > target)
+                return;
+            else
+            {
+                for (int i = start; i < candidates.Length; i++)
+                {
+                    temp.Add(candidates[i]);
+                    BackTrack(candidates, target, result, temp, i);
+                    temp.RemoveAt(temp.Count - 1);
+                }
+            }
+        }
+    }
+}
