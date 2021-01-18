ec391916036a831e6c487c99f9b8cc90db1317b0
Woei Loon <woeiloon96@gmail.com>
2021-01-19 06:41:51 +0800

    90. Subsets II

---
+++ LeetCode.Test/Q1-100/Q90Test.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Q1_100;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Test.Q1_100
+{
+    class Q90Test
+    {
+        [Test]
+        public void Test()
+        {
+            IList<IList<int>> result = new Q90().SubsetsWithDup(new[] { 1, 2, 2 });
+
+            IList<IList<int>> expected = new List<IList<int>>
+            {
+                new List<int> { },
+                new List<int> { 1 },
+                new List<int> { 2 },
+                new List<int> { 1, 2 },
+                new List<int> { 1, 2, 2 },
+                new List<int> { 2, 2 },
+            };
+
+            CollectionAssert.AreEquivalent(expected, result);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q90.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Q1_100
+{
+    public class Q90
+    {
+        public IList<IList<int>> SubsetsWithDup(int[] nums)
+        {
+            Array.Sort(nums);
+            IList<IList<int>> result = new List<IList<int>>();
+            BackTrack(nums, result, new List<int>(), 0);
+            return result;
+        }
+
+        private void BackTrack(int[] nums, IList<IList<int>> result, IList<int> temp, int start)
+        {
+            result.Add(new List<int>(temp));
+
+            for (int i = start; i < nums.Length; i++)
+            {
+                if (i > start && nums[i - 1] == nums[i])
+                    continue;
+
+                temp.Add(nums[i]);
+                BackTrack(nums, result, temp, i + 1);
+                temp.RemoveAt(temp.Count - 1);
+            }
+        }
+    }
+}
