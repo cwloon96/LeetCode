add2968fcccdc7d9f19dabd6fa3134356abd826c
Woei Loon <woeiloon96@gmail.com>
2021-01-20 06:45:49 +0800

    Revision Q1282

--- LeetCode.Test/Q1201-1300/Q1282Test.cs
+++ LeetCode.Test/Q1201-1300/Q1282Test.cs
@@ -18,7 +18,22 @@ namespace LeetCode.Test.Q1201_1300
                 new List<int> { 3, 4, 6 },
             };
 
-            Assert.AreEqual(expected, result);
+            CollectionAssert.AreEquivalent(expected, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            IList<IList<int>> result = new Q1282().GroupThePeople(new[] { 2, 1, 3, 3, 3, 2 });
+
+            List<IList<int>> expected = new List<IList<int>>
+            {
+                new List<int> { 1 },
+                new List<int> { 0, 5 },
+                new List<int> { 2, 3, 4 },
+            };
+
+            CollectionAssert.AreEquivalent(expected, result);
         }
     }
 }
