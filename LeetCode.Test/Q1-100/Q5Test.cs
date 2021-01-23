9a74a09c389d3a27d02ee37e6d41dc6f50f0647e
Woei Loon <woeiloon96@gmail.com>
2021-01-23 09:21:00 +0800

    5. Longest Palindromic Substring

---
+++ LeetCode.Test/Q1-100/Q5Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q1_100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1_100
+{
+    class Q5Test
+    {
+        [Test]
+        public void Test()
+        {
+            string result = new Q5().LongestPalindrome("babad");
+
+            Assert.AreEqual("bab", result);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q5.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Q1_100
+{
+    public class Q5
+    {
+        public string LongestPalindrome(string s)
+        {
+            string result = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                string odd = GetPalindrome(i, i, s);
+                string even = GetPalindrome(i, i + 1, s);
+
+                string current = odd.Length > even.Length ? odd : even;
+                result = current.Length > result.Length ? current : result;
+            }
+
+            return result;
+        }
+
+        private string GetPalindrome(int left, int right, string s)
+        {
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                left--;
+                right++;
+            }
+
+            return s.Substring(left + 1, right - 1 - left);
+        }
+    }
+}
