2cd4be23f5696676f5683895fab8db1dc5c6c1cf
Woei Loon <woeiloon96@gmail.com>
2021-01-24 06:58:36 +0800

    125. Valid Palindrome

---
+++ LeetCode.Test/Q101-200/Q125Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q101_200;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q101_200
+{
+    class Q125Test
+    {
+        [Test]
+        public void Test()
+        {
+            bool result = new Q125().IsPalindrome("A man, a plan, a canal: Panama");
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ LeetCode/Q101-200/Q125.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Q101_200
+{
+    public class Q125
+    {
+        public bool IsPalindrome(string s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right)
+            {
+                if (!IsValid(s[left]) && left < s.Length - 1)
+                {
+                    left++;
+                    continue;
+                }
+
+                if (!IsValid(s[right]) && right > 0)
+                {
+                    right--;
+                    continue;
+                }
+
+                if (char.ToLower(s[left]) != char.ToLower(s[right]))
+                    return false;
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+
+        private bool IsValid(char c)
+        {
+            return char.IsDigit(c) || char.IsLetter(c);
+        }
+    }
+}
