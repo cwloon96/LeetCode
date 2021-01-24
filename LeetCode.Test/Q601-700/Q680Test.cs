d3dd2154f4d83bddabc81969dfb27fae4d6003ee
Woei Loon <woeiloon96@gmail.com>
2021-01-25 06:44:11 +0800

    680. Valid Palindrome II

---
+++ LeetCode.Test/Q601-700/Q680Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q601_700;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q601_700
+{
+    class Q680Test
+    {
+        [Test]
+        public void Test()
+        {
+            bool result = new Q680().ValidPalindrome("cbbcc");
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ LeetCode/Q601-700/Q680.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Q601_700
+{
+    public class Q680
+    {
+        public bool ValidPalindrome(string s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right && left < s.Length && right >= 0)
+            {
+                if (s[left] != s[right])
+                    return IsPalindrome(s.Substring(left, right - left)) ||
+                        IsPalindrome(s.Substring(left + 1, right - left));
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+
+        private bool IsPalindrome(string s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right && left < s.Length && right >= 0 && s[left] == s[right])
+            {
+                left++;
+                right--;
+            }
+
+            return left >= right;
+        }
+    }
+}
