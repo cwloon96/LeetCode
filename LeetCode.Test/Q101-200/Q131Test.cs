f6b75d9a814b675b46c2aca6660f2cf800ca7f93
Woei Loon <woeiloon96@gmail.com>
2021-01-26 06:54:41 +0800

    131. Palindrome Partitioning

---
+++ LeetCode.Test/Q101-200/Q131Test.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Q101_200;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Test.Q101_200
+{
+    class Q131Test
+    {
+        [Test]
+        public void Test()
+        {
+            IList<IList<string>> result = new Q131().Partition("aab");
+            IList<IList<string>> expected = new List<IList<string>>
+            {
+                new List<string> { "a", "a", "b"},
+                new List<string> { "aa", "b"}
+            };
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ LeetCode/Q101-200/Q131.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Q101_200
+{
+    public class Q131
+    {
+        public IList<IList<string>> Partition(string s)
+        {
+            IList<IList<string>> result = new List<IList<string>>();
+            BackTracking(s, result, new List<string>());
+            return result;
+        }
+
+        private void BackTracking(string s, IList<IList<string>> result, List<string> temp)
+        {
+            if (s.Length == 0)
+                result.Add(new List<string>(temp));
+            else
+            {
+                for (int length = 1; length <= s.Length; length++)
+                {
+                    string current = s.Substring(0, length);
+                    if (IsPalindrome(current))
+                    {
+                        temp.Add(current);
+                        BackTracking(s.Substring(length), result, temp);
+                        temp.RemoveAt(temp.Count - 1);
+                    }
+                }
+            }
+        }
+
+        private bool IsPalindrome(string s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                    return false;
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+}
