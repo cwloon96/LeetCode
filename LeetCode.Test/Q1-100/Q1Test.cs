9ee76202f0d389d5f810e1cc63e8afcdea6aaee2
Woei Loon <woeiloon96@gmail.com>
2021-01-27 06:40:29 +0800

    1. Two Sum

---
+++ LeetCode.Test/Q1-100/Q1Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q1_100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1_100
+{
+    class Q1Test
+    {
+        [Test]
+        public void Test()
+        {
+            int[] result = new Q1().TwoSum(new[] { 2, 7, 11, 15 }, 9);
+
+            Assert.AreEqual(new int[2] { 0, 1 }, result);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Q1_100
+{
+    public class Q1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            int[] result = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int current = nums[i];
+                int remain = target - current;
+
+                if (dict.ContainsKey(remain))
+                {
+                    result[0] = dict[remain];
+                    result[1] = i;
+                    break;
+                }
+
+                dict.Add(current, i);
+
+            }
+
+            return result;
+        }
+    }
+}
