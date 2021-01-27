8b14bc428ce1b64fbe3658b5f79229c903457c08
Woei Loon <woeiloon96@gmail.com>
2021-01-28 07:02:39 +0800

    21. Merge Two Sorted Lists

---
+++ LeetCode.Test/Q1-100/Q21Test.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Common.Models;
+using LeetCode.Q1_100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1_100
+{
+    class Q21Test
+    {
+        [Test]
+        public void Test()
+        {
+            ListNode l1 = new ListNode(1);
+            l1.next = new ListNode(2);
+            l1.next.next = new ListNode(4);
+
+            ListNode l2 = new ListNode(1);
+            l2.next = new ListNode(3);
+            l2.next.next = new ListNode(4);
+
+            ListNode result = new Q21().MergeTwoLists(l1, l2);
+            Assert.AreEqual(1, result.val);
+            Assert.AreEqual(1, result.next.val);
+            Assert.AreEqual(2, result.next.next.val);
+            Assert.AreEqual(3, result.next.next.next.val);
+            Assert.AreEqual(4, result.next.next.next.next.val);
+            Assert.AreEqual(4, result.next.next.next.next.next.val);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q21.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Common.Models;
+
+namespace LeetCode.Q1_100
+{
+    public class Q21
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null && l2 == null)
+                return null;
+
+            if (l1 == null || (l1 != null && l2 != null && l1.val > l2.val))
+                return new ListNode(l2.val, MergeTwoLists(l1, l2?.next));
+            else
+                return new ListNode(l1.val, MergeTwoLists(l1.next, l2));
+        }
+    }
+}
