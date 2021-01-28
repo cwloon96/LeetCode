0d785e27e1fbdd8b01cef067fb536677dbc90192
Woei Loon <woeiloon96@gmail.com>
2021-01-29 06:56:50 +0800

    647. Palindromic Substrings

---
+++ LeetCode.Test/Q601-700/Q647Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q601_700;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q601_700
+{
+    class Q647Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q647().CountSubstrings("aaa");
+            Assert.AreEqual(6, result);
+        }
+    }
+}
---
+++ LeetCode/Q601-700/Q647.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Q601_700
+{
+    public class Q647
+    {
+        public int CountSubstrings(string s)
+        {
+            int result = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                result += CountPalindrome(s, i, i);
+                result += CountPalindrome(s, i, i + 1);
+            }
+
+            return result;
+        }
+
+        private int CountPalindrome(string s, int left, int right)
+        {
+            int result = 0;
+
+            while (left >= 0 && right < s.Length && s[left--] == s[right++])
+                result++;
+
+            return result;
+        }
+    }
+}
