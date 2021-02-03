8f246308c2004458ccb35c3aafac11675bcdc98c
Woei Loon <woeiloon96@gmail.com>
2021-02-04 07:04:07 +0800

    revision Q15

--- LeetCode.Test/Q1-100/Q15Test.cs
+++ LeetCode.Test/Q1-100/Q15Test.cs
@@ -10,8 +10,18 @@ namespace LeetCode.Test.Q1_100
         public void Test()
         {
             var result = new Q15().ThreeSum(new[] { -1, 0, 1, 2, -1, -4 });
+            var expected = new List<IList<int>> { new List<int> { -1, -1, 2 }, new List<int> { -1, 0, 1 } };
 
-            Assert.AreEqual(new List<IList<int>> { new List<int> { -1, -1, 2 }, new List<int> { -1, 0, 1 } }, result);
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var result = new Q15().ThreeSum(new int[0]);
+            var expected = new List<IList<int>> { };
+
+            CollectionAssert.AreEqual(expected, result);
         }
     }
 }
