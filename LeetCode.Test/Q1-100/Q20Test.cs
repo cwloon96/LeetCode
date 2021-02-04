099d6d659ef14a2d8129dc9377be603fd7b721b3
Woei Loon <woeiloon96@gmail.com>
2021-02-05 07:13:48 +0800

    20. Valid Parentheses

---
+++ LeetCode.Test/Q1-100/Q20Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q1_100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1_100
+{
+    public class Q20Test
+    {
+        [Test]
+        public void Test()
+        {
+            bool result = new Q20().IsValid("()[]{}");
+
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q20.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Q1_100
+{
+    public class Q20
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char current = s[i];
+                if (current == '(' || current == '{' || current == '[')
+                {
+                    stack.Push(current);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                        return false;
+
+                    char pop = stack.Pop();
+                    if (pop == '(' && current != ')')
+                        return false;
+                    if (pop == '{' && current != '}')
+                        return false;
+                    if (pop == '[' && current != ']')
+                        return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
