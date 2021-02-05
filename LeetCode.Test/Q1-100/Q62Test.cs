c2bbface32df5c21251873817ef21d2bf8ea6ac6
Woei Loon <woeiloon96@gmail.com>
2021-02-06 07:00:29 +0800

    62. Unique Paths

---
+++ LeetCode.Test/Q1-100/Q62Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q1_100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1_100
+{
+    class Q62Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q62().UniquePaths(7, 3);
+
+            Assert.AreEqual(28, result);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q62.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Q1_100
+{
+    public class Q62
+    {
+        public int UniquePaths(int m, int n)
+        {
+            int[] dp = new int[n];
+
+            for (int i = 0; i < n; i++)
+                dp[i] = 1;
+
+            for (int row = 1; row < m; row++)
+                for (int col = 1; col < n; col++)
+                    dp[col] += dp[col - 1];
+
+            return dp[n - 1];
+        }
+    }
+}
