7d1c7b9392197330e5659953eb973e086f474ca2
Woei Loon <woeiloon96@gmail.com>
2021-02-07 07:03:03 +0800

    509. Fibonacci Number

---
+++ LeetCode.Test/Q501-600/Q509Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q501_600;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q501_600
+{
+    class Q509Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q509().Fib(3);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
