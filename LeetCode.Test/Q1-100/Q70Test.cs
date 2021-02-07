b7eb245d871d14c1b702b6387cbf644c84c06f73
Woei Loon <woeiloon96@gmail.com>
2021-02-08 06:36:03 +0800

    70. Climbing Stairs

---
+++ LeetCode.Test/Q1-100/Q70Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q1_100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1_100
+{
+    public class Q70Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q70().ClimbStairs(5);
+
+            Assert.AreEqual(8, result);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q70.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Q1_100
+{
+    public class Q70
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n <= 2)
+                return n;
+
+            int oneStepBefore = 2;
+            int twoStepBefore = 1;
+            int result = 0;
+
+            for (int i = 2; i < n; i++)
+            {
+                result = oneStepBefore + twoStepBefore;
+                twoStepBefore = oneStepBefore;
+                oneStepBefore = result;
+            }
+
+            return result;
+        }
+    }
+}
