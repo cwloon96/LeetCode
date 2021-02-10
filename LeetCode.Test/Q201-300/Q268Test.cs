5f4cd5a8830a792e21599688c60b940f338462b0
Woei Loon <woeiloon96@gmail.com>
2021-02-11 07:08:54 +0800

    revision Q268

--- LeetCode.Test/Q201-300/Q268Test.cs
+++ LeetCode.Test/Q201-300/Q268Test.cs
@@ -11,5 +11,12 @@ namespace LeetCode.Test.Q201_300
             int result = new Q268().MissingNumber(new[] { 3, 0, 1 });
             Assert.AreEqual(2, result);
         }
+
+        [Test]
+        public void Test2()
+        {
+            int result = new Q268().MissingNumber(new[] { 0, 1 });
+            Assert.AreEqual(2, result);
+        }
     }
 }
