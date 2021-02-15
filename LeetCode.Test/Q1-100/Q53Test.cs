161b7a1f5f67023472961ef070a51463bb9c24cf
Woei Loon <woeiloon96@gmail.com>
2021-02-16 07:46:15 +0800

    revision Q53

--- LeetCode.Test/Q1-100/Q53Test.cs
+++ LeetCode.Test/Q1-100/Q53Test.cs
@@ -6,7 +6,7 @@ namespace LeetCode.Test.Q1_100
     class Q53Test
     {
         [Test]
-        public void Test()
+        public void TestDP()
         {
             int result = new Q53().MaxSubArrayDP(new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
 
@@ -14,11 +14,27 @@ namespace LeetCode.Test.Q1_100
         }
 
         [Test]
-        public void Test2()
+        public void TestDP2()
+        {
+            int result = new Q53().MaxSubArrayDP(new[] { 4 });
+
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void TestDAC()
         {
             int result = new Q53().MaxSubArrayDAC(new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
 
             Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void TestDAC2()
+        {
+            int result = new Q53().MaxSubArrayDAC(new[] { 4 });
+
+            Assert.AreEqual(4, result);
         }
     }
 }
