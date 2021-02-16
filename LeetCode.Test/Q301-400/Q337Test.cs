fd0c52baf7311152a448bcf64916348894720a4c
Woei Loon <woeiloon96@gmail.com>
2021-02-17 06:42:48 +0800

    revision Q337

--- LeetCode.Test/Q301-400/Q337Test.cs
+++ LeetCode.Test/Q301-400/Q337Test.cs
@@ -19,5 +19,20 @@ namespace LeetCode.Test.Q301_400
             int result = new Q337().Rob(root);
             Assert.AreEqual(7, result);
         }
+
+        [Test]
+        public void Test2()
+        {
+            TreeNode root = new TreeNode(3);
+            root.left = new TreeNode(4);
+            root.left.left = new TreeNode(1);
+            root.left.right = new TreeNode(3);
+
+            root.right = new TreeNode(5);
+            root.right.right = new TreeNode(1);
+
+            int result = new Q337().Rob(root);
+            Assert.AreEqual(9, result);
+        }
     }
 }
