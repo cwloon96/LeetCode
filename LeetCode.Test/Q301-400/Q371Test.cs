eb47296bfdc31dafbeb87793b318b2d474edb1ab
Woei Loon <woeiloon96@gmail.com>
2021-02-18 07:05:03 +0800

    371. Sum of Two Integers

---
+++ LeetCode.Test/Q301-400/Q371Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q301_400;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q301_400
+{
+    class Q371Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q371().GetSum(1, 2);
+
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ LeetCode/Q301-400/Q371.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Q301_400
+{
+    public class Q371
+    {
+        public int GetSum(int a, int b)
+        {
+            /* in base 10 : 373 + 338 = 711
+             * add without carry -> 601
+             * add carry only -> 110
+             * 601 + 110 = 711
+            */
+
+            /* in binary : 111 + 010 = 1001
+             * add without carry -> 101 (XOR) a ^ b
+             * add carry only -> 100 (AND, then shift 1 to left) (a & b) << 1
+             * 101 + 100 = 1001
+            */
+
+            return b == 0 ? a : GetSum(a ^ b, (a & b) << 1);
+        }
+    }
+}
