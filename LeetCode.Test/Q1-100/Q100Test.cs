0b216ca8c0b827b10db9650440e1ba22273eb293
Woei Loon <woeiloon96@gmail.com>
2021-02-19 06:43:36 +0800

    100. Same Tree

---
+++ LeetCode.Test/Q1-100/Q100Test.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Common.Models;
+using LeetCode.Q1_100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1_100
+{
+    class Q100Test
+    {
+        [Test]
+        public void Test()
+        {
+            TreeNode treeA = new TreeNode(1);
+            treeA.left = new TreeNode(2);
+            treeA.right = new TreeNode(3);
+
+            TreeNode treeB = new TreeNode(1);
+            treeB.left = new TreeNode(2);
+            treeB.right = new TreeNode(3);
+
+            bool result = new Q100().IsSameTree(treeA, treeB);
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q100.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Common.Models;
+
+namespace LeetCode.Q1_100
+{
+    public class Q100
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+
+            if (p == null || q == null)
+                return false;
+
+            if (!IsSameTree(p.left, q.left)) return false;
+            if (!IsSameTree(p.right, q.right)) return false;
+
+            return p.val == q.val;
+        }
+    }
+}
