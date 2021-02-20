6ecd06e00463c13e500c446c6854878663c27722
Woei Loon <woeiloon96@gmail.com>
2021-02-21 07:35:47 +0800

    1470. Shuffle the Array

---
+++ LeetCode.Test/Q1401-1500/Q1470Test.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Q1401_1500;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1401_1500
+{
+    class Q1470Test
+    {
+        [Test]
+        public void Test()
+        {
+            int[] result = new Q1470().Shuffle(new[] { 2, 5, 1, 3, 4, 7 }, 3);
+
+            int[] expected = new[] { 2, 3, 5, 4, 1, 7 };
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ LeetCode/Q1401-1500/Q1470.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Q1401_1500
+{
+    public class Q1470
+    {
+        public int[] Shuffle(int[] nums, int n)
+        {
+            int[] result = new int[nums.Length];
+
+            // each time will add two element to result array, thus i += 2
+            // j is to loop the original array, thus j++
+            for (int i = 0, j = 0; j < n; i += 2, j++)
+            {
+                result[i] = nums[j];
+                result[i + 1] = nums[j + n];
+            }
+
+            return result;
+        }
+    }
+}
