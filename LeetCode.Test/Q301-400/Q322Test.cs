d4c84b341e221f6cdad9139c4de0fb025bcd5cf3
Woei Loon <woeiloon96@gmail.com>
2021-02-20 09:16:33 +0800

    322. Coin Change

---
+++ LeetCode.Test/Q301-400/Q322Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q301_400;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q301_400
+{
+    class Q322Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q322().CoinChange(new[] { 1, 2, 5 }, 11);
+
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ LeetCode/Q301-400/Q322.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LeetCode.Q301_400
+{
+    public class Q322
+    {
+        public int CoinChange(int[] coins, int amount)
+        {
+            if (amount == 0) return 0;
+
+            // index = amount, value = min coin needed to reach the amount
+            int[] dp = new int[amount + 1];
+                
+            Array.Fill(dp, int.MaxValue);
+            // if the amount is 0, value always 0
+            dp[0] = 0;
+
+            // value will keep updating according to the coin value
+            foreach (int coin in coins)
+            {
+                for (int i = coin; i <= amount; i++)
+                {
+                    // every first time enter to this loop, i - coin must be 0, so the first value can always enter to this if statement
+                    if (dp[i - coin] != int.MaxValue)
+                    {
+                        // (i - coin) = the minimum coin needed to reach the nearest value which is the current amount - current coin value
+                        dp[i] = Math.Min(dp[i], dp[i - coin] + 1);
+                    }
+                }
+            }
+
+            return dp[amount] == int.MaxValue ? -1 : dp[amount];
+        }
+    }
+}
