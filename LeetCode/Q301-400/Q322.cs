d4eb7c3a06b903abba1be3f9d2aa2fe3954062c4
Woei Loon <woeiloon96@gmail.com>
2021-02-20 09:47:55 +0800

    update Q322 comment

--- LeetCode/Q301-400/Q322.cs
+++ LeetCode/Q301-400/Q322.cs
@@ -8,14 +8,21 @@ namespace LeetCode.Q301_400
         {
             if (amount == 0) return 0;
 
-            // index = amount, value = min coin needed to reach the amount
+            // index = amount, value = min coin needed to reach the amount (for all coin looped, not only current coin)
             int[] dp = new int[amount + 1];
                 
             Array.Fill(dp, int.MaxValue);
             // if the amount is 0, value always 0
             dp[0] = 0;
 
-            // value will keep updating according to the coin value
+            /* Eg: coins = [1, 2, 5], amount = 11
+             * 1st loop: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+             * 2nd loop: [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6]
+             * 3rd loop: [0, 1, 1, 2, 2, 1, 2, 2, 3, 3, 2, 3]
+             * in second loop, dp[3] = 2 came from dp[3 - 2] + 1, the result means 1 coin (value 1) + 1 coin (value 2) = 3, thus 2 coin
+            */
+
+            // dp value will keep updating according to the coin value
             foreach (int coin in coins)
             {
                 for (int i = coin; i <= amount; i++)
