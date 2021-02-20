f33fc8d3eb61c14e6334d4f61441141c0c381723
Woei Loon <woeiloon96@gmail.com>
2021-02-21 06:56:15 +0800

    Q1108

---
+++ LeetCode.Test/Q1101-1200/Q1108Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q1101_1200;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1101_1200
+{
+    class Q1108Test
+    {
+        [Test]
+        public void Test()
+        {
+            string result = new Q1108().DefangIPaddr("1.1.1.1");
+            Assert.AreEqual("1[.]1[.]1[.]1", result);
+        }
+    }
+}
---
+++ LeetCode/Q1101-1200/Q1108.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace LeetCode.Q1101_1200
+{
+    public class Q1108
+    {
+        public string DefangIPaddr(string address)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in address)
+            {
+                if (c == '.')
+                    sb.Append("[.]");
+                else
+                    sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
