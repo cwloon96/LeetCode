717ac200cacf99ffdce120cc32145633f021a9e8
Woei Loon <woeiloon96@gmail.com>
2021-02-22 07:08:16 +0800

    1672. Richest Customer Wealth

---
+++ LeetCode.Test/Q1601-1700/Q1672Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q1601_1700;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1601_1700
+{
+    class Q1672Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q1672().MaximumWealth(new[] { new[] { 1, 2, 3 }, new[] { 1, 2, 3 } });
+
+            Assert.AreEqual(6, result);
+        }
+    }
+}
---
+++ LeetCode/Q1601-1700/Q1672.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode.Q1601_1700
+{
+    public class Q1672
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            int result = 0;
+
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                int current = 0;
+                for (int j = 0; j < accounts[i].Length; j++)
+                    current += accounts[i][j];
+
+                result = Math.Max(result, current);
+            }
+
+            return result;
+        }
+    }
+}
