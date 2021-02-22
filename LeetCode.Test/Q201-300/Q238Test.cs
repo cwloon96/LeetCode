85a07064853b911df337ed970508e646688832c8
Woei Loon <woeiloon96@gmail.com>
2021-02-23 07:00:46 +0800

    238. Product of Array Except Self

---
+++ LeetCode.Test/Q201-300/Q238Test.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Q201_300;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q201_300
+{
+    class Q238Test
+    {
+        [Test]
+        public void Test()
+        {
+            int[] result = new Q238().ProductExceptSelf(new[] { 1, 2, 3, 4 });
+
+            int[] expected = new[] { 24, 12, 8, 6 };
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ LeetCode/Q201-300/Q238.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Q201_300
+{
+    public class Q238
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+
+            /* Eg: [2, 3, 4, 5]
+             * first loop, result = 
+             * [1, 
+             * 2, 
+             * 2 * 3, 
+             * 2 * 3 * 4] -> [1, 2, 6, 24]
+             * second loop, result = 
+             * [1 * 5 * 4 * 3 , 
+             * 2 * 5 * 4, 
+             * 6 * 5,
+             * 24 * 1] -> [60, 40, 30, 24]
+            */
+
+            // temp = multiplied value of all nums before the current num
+            for (int i = 0, temp = 1; i < nums.Length; i++)
+            {
+                result[i] = temp;
+                temp *= nums[i];
+            }
+
+            for (int i = nums.Length - 1, temp = 1; i >= 0; i--)
+            {
+                result[i] *= temp;
+                temp *= nums[i];
+            }
+
+            return result;
+        }
+    }
+}
