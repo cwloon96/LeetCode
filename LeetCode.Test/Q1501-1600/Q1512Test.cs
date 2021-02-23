76b08680345ab690568e119c922f22d55bf9d4dd
Woei Loon <woeiloon96@gmail.com>
2021-02-24 06:34:58 +0800

    1512. Number of Good Pairs

---
+++ LeetCode.Test/Q1501-1600/Q1512Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q1501_1600;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1501_1600
+{
+    class Q1512Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q1512().NumIdenticalPairs(new[] { 1, 2, 3, 1, 1, 3 });
+            Assert.AreEqual(4, result);
+        }
+    }
+}
---
+++ LeetCode/Q1501-1600/Q1512.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Q1501_1600
+{
+    public class Q1512
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int result = 0;
+            int[] arr = new int[101];
+
+            // we only need to find how many time the num appear before the current num
+            /* Eg: [1, 2, 1]
+             * i = 0, result = 0, arr[1] = 1
+             * i = 1, result = 0, arr[2] = 1
+             * i = 2, result = 1, arr[1] = 2
+            */
+            for (int i = 0; i < nums.Length; i++)
+                result += arr[nums[i]]++;
+
+            return result;
+        }
+    }
+}
