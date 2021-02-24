0e2d4bad8605d6915c44243b643f5ab46f4b797e
Woei Loon <woeiloon96@gmail.com>
2021-02-25 06:56:40 +0800

    1768. Merge Strings Alternately

---
+++ LeetCode.Test/Q1701-1800/Q1768Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q1701_1800;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1701_1800
+{
+    class Q1768Test
+    {
+        [Test]
+        public void Test()
+        {
+            string result = new Q1768().MergeAlternately("abc", "pqr");
+            Assert.AreEqual("apbqcr", result);
+        }
+    }
+}
---
+++ LeetCode/Q1701-1800/Q1768.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode.Q1701_1800
+{
+    public class Q1768
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            StringBuilder sb = new StringBuilder();
+            int max = Math.Max(word1.Length, word2.Length);
+
+            for (int i = 0; i < max; i++)
+            {
+                if (i < word1.Length)
+                    sb.Append(word1[i]);
+
+                if (i < word2.Length)
+                    sb.Append(word2[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
