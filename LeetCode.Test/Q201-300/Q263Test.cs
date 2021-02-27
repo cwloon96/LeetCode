638e56ddd557594c792bbc771bd02e359ae3f49d
Woei Loon <woeiloon96@gmail.com>
2021-02-28 06:54:16 +0800

    263. Ugly Number

---
+++ LeetCode.Test/Q201-300/Q263Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q201_300;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q201_300
+{
+    class Q263Test
+    {
+        [Test]
+        public void Test()
+        {
+            bool result = new Q263().IsUgly(6);
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ LeetCode/Q201-300/Q263.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Q201_300
+{
+    public class Q263
+    {
+        public bool IsUgly(int num)
+        {
+            if (num < 1) return false;
+
+            foreach (int divisor in new[] { 2, 3, 5 })
+                while (num % divisor == 0)
+                    num /= divisor;
+
+            return num == 1;
+        }
+    }
+}
