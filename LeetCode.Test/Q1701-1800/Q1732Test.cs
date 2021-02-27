54396046abc0c6db7b25b42fab695a55b05ca508
Woei Loon <woeiloon96@gmail.com>
2021-02-28 06:25:54 +0800

    1732. Find the Highest Altitude

---
+++ LeetCode.Test/Q1701-1800/Q1732Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q1701_1800;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1701_1800
+{
+    class Q1732Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q1732().LargestAltitude(new[] { -5, 1, 5, 0, -7 });
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ LeetCode/Q1701-1800/Q1732.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCode.Q1701_1800
+{
+    public class Q1732
+    {
+        public int LargestAltitude(int[] gain)
+        {
+            int max = 0;
+            int current = 0;
+
+            for (int i = 0; i < gain.Length; i++)
+            {
+                current += gain[i];
+                max = Math.Max(max, current);
+            }
+
+            return max;
+        }
+    }
+}
