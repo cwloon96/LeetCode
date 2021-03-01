f5bcd283904553bcad3ff340af9a2ceb3d85fccf
Woei Loon <woeiloon96@gmail.com>
2021-03-02 06:35:18 +0800

    1736. Latest Time by Replacing Hidden Digits

---
+++ LeetCode.Test/Q1701-1800/Q1736Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q1701_1800;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1701_1800
+{
+    class Q1736Test
+    {
+        [Test]
+        public void Test()
+        {
+            string result = new Q1736().MaximumTime("2?:?0");
+            Assert.AreEqual("23:50", result);
+        }
+    }
+}
---
+++ LeetCode/Q1701-1800/Q1736.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace LeetCode.Q1701_1800
+{
+    public class Q1736
+    {
+        public string MaximumTime(string time)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (time[0] == '?')
+                if (time[1] == '?' || time[1] <= '3')
+                    sb.Append("2");
+                else
+                    sb.Append("1");
+            else
+                sb.Append(time[0]);
+
+            if (time[1] == '?')
+                if (time[0] == '?' || time[0] == '2')
+                    sb.Append("3");
+                else
+                    sb.Append("9");
+            else
+                sb.Append(time[1]);
+
+            sb.Append(":");
+
+            if (time[3] == '?')
+                sb.Append("5");
+            else
+                sb.Append(time[3]);
+
+            if (time[4] == '?')
+                sb.Append("9");
+            else
+                sb.Append(time[4]);
+
+            return sb.ToString();
+        }
+    }
+}
