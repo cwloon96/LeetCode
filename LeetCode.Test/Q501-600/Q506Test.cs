76474b4b87b58457e21432c3fc8137dae64a9e52
Woei Loon <woeiloon96@gmail.com>
2021-03-03 06:58:02 +0800

    506. Relative Ranks

---
+++ LeetCode.Test/Q501-600/Q506Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q501_600;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q501_600
+{
+    class Q506Test
+    {
+        [Test]
+        public void Test()
+        {
+            string[] result = new Q506().FindRelativeRanks(new[] { 10, 3, 8, 9, 4 });
+            string[] expected = new[] { "Gold Medal", "5", "Bronze Medal", "Silver Medal", "4" };
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ LeetCode/Q501-600/Q506.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Q501_600
+{
+    public class Q506
+    {
+        public string[] FindRelativeRanks(int[] score)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            int length = score.Length;
+
+            // add value -> index into map
+            for (int i = 0; i < length; i++)
+                map.Add(score[i], i);
+
+            // sort them to get ranking
+            Array.Sort(score);
+            string[] result = new string[length];
+
+            for (int rankIndex = 0; rankIndex < length; rankIndex++)
+            {
+                // use rankIndex to trace the current ranking
+                // 0 means first rank which is the last element, thus length - 1 - rankIndex to get last element
+                int index = map[score[length - 1 - rankIndex]];
+
+                if (rankIndex == 0)
+                    result[index] = "Gold Medal";
+                else if (rankIndex == 1)
+                    result[index] = "Silver Medal";
+                else if (rankIndex == 2)
+                    result[index] = "Bronze Medal";
+                else
+                    result[index] = (rankIndex + 1).ToString();
+            }
+
+            return result;
+        }
+    }
+}
