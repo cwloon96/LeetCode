44b7298a19eae16494bd898ab9a226701040d80b
Woei Loon <woeiloon96@gmail.com>
2021-03-04 06:57:36 +0800

    455. Assign Cookies

---
+++ LeetCode.Test/Q401-500/Q455Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q401_500;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q401_500
+{
+    class Q455Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q455().FindContentChildren(new[] { 2, 1 }, new[] {1,2,3 });
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ LeetCode/Q401-500/Q455.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode.Q401_500
+{
+    public class Q455
+    {
+        public int FindContentChildren(int[] g, int[] s)
+        {
+            Array.Sort(g);
+            Array.Sort(s);
+
+            int result = 0;
+            for (int i = 0; i < s.Length && result < g.Length; i++)
+                if (g[result] <= s[i])
+                    result++;
+
+            return result;
+        }
+    }
+}
