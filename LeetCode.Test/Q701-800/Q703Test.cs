214d122d1f3aac0bcaf6f58d995b95f026aff3ad
Woei Loon <woeiloon96@gmail.com>
2021-03-05 12:44:12 +0800

    703. Kth Largest Element in a Stream

---
+++ LeetCode.Test/Q701-800/Q703Test.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Q701_800;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q701_800
+{
+    class Q703Test
+    {
+        [Test]
+        public void Test()
+        {
+            var obj = new KthLargest(2, new[] { 0 });
+            Assert.AreEqual(-1, obj.Add(-1));
+            Assert.AreEqual(0, obj.Add(1));
+            Assert.AreEqual(0, obj.Add(-2));
+            Assert.AreEqual(0, obj.Add(-4));
+            Assert.AreEqual(1, obj.Add(3));
+        }
+    }
+}
---
+++ LeetCode/Q701-800/Q703.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Q701_800
+{
+    public class KthLargest
+    {
+        private int target;
+        private int size;
+        private SortedDictionary<int, int> dict = new SortedDictionary<int, int>();
+
+        public KthLargest(int k, int[] nums)
+        {
+            target = k;
+            foreach (var num in nums)
+                Add(num);
+        }
+
+        public int Add(int val)
+        {
+            if (dict.ContainsKey(val))
+                dict[val]++;
+            else
+                dict.Add(val, 1);
+            size++;
+
+            if (size > target)
+            {
+                var first = dict.First();
+                if (first.Value == 1)
+                    dict.Remove(first.Key);
+                else
+                    dict[first.Key]--;
+                size--;
+            }
+
+            return dict.First().Key;
+        }
+    }
+}
