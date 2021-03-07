551d2a3096c705e0de2f6f1fa0ca32d1182cf580
Woei Loon <woeiloon96@gmail.com>
2021-03-08 06:41:22 +0800

    500. Keyboard Row

---
+++ LeetCode.Test/Q401-500/Q500Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q401_500;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q401_500
+{
+    class Q500Test
+    {
+        [Test]
+        public void Test()
+        {
+            string[] result = new Q500().FindWords(new[] { "Hello", "Alaska", "Dad", "Peace" });
+            string[] expected = new[] { "Alaska", "Dad" };
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ LeetCode/Q401-500/Q500.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Q401_500
+{
+    public class Q500
+    {
+        public string[] FindWords(string[] words)
+        {
+            string[] keyboard = new[] { "qwertyuiop", "asdfghjkl", "zxcvbnm" };
+            Dictionary<char, int> keyRow = new Dictionary<char, int>();
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < keyboard.Length; i++)
+                foreach (var key in keyboard[i])
+                    keyRow.Add(key, i);
+
+            foreach (var word in words)
+            {
+                int row = keyRow[Char.ToLower(word[0])];
+                int i = 1;
+                for (; i < word.Length; i++)
+                    if (keyRow[Char.ToLower(word[i])] != row)
+                        break;
+
+                if (i == word.Length)
+                    result.Add(word);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
