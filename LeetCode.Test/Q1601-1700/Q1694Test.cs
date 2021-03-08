bf8fdf9d9e026ddaf9d9418258a974d47d6839c7
Woei Loon <woeiloon96@gmail.com>
2021-03-09 06:50:24 +0800

    1694. Reformat Phone Number

---
+++ LeetCode.Test/Q1601-1700/Q1694Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q1601_1700;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1601_1700
+{
+    class Q1694Test
+    {
+        [Test]
+        public void Test()
+        {
+            string result = new Q1694().ReformatNumber("123 4-567");
+
+            Assert.AreEqual("123-45-67", result);
+        }
+    }
+}
---
+++ LeetCode/Q1601-1700/Q1694.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace LeetCode.Q1601_1700
+{
+    public class Q1694
+    {
+        public string ReformatNumber(string number)
+        {
+            StringBuilder sb = new StringBuilder();
+            int index = 0;
+            number = number.Replace("-", "").Replace(" ", "");
+
+            while (index < number.Length)
+            {
+                int digitLeft = number.Length - index;
+                if (digitLeft > 4)
+                {
+                    sb.Append(number.Substring(index, 3));
+                    index += 3;
+                    sb.Append("-");
+                }
+                else if (digitLeft == 4)
+                {
+                    sb.Append(number.Substring(index, 2));
+                    index += 2;
+                    sb.Append("-");
+
+                    sb.Append(number.Substring(index, 2));
+                    index += 2;
+                    if (index < number.Length)
+                        sb.Append("-");
+                }
+                else
+                {
+                    sb.Append(number.Substring(index));
+                    index += digitLeft;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
