10a98a75a2ac79caeb60c5faa64b731d95f6f2a5
Woei Loon <woeiloon96@gmail.com>
2021-03-10 06:44:27 +0800

    496. Next Greater Element I

---
+++ LeetCode.Test/Q401-500/Q496Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q401_500;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q401_500
+{
+    class Q496Test
+    {
+        [Test]
+        public void Test()
+        {
+            int[] result = new Q496().NextGreaterElement(new[] { 4, 1, 2 }, new[] { 1, 3, 4, 2 });
+            int[] expected = new[] { -1, 3, -1 };
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ LeetCode/Q401-500/Q496.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Q401_500
+{
+    // Better description:
+    // take an element in the first array and then find the same element in the second array and then look to the right in the second array to find a greater one.
+    public class Q496
+    {
+        public int[] NextGreaterElement(int[] nums1, int[] nums2)
+        {
+            Stack<int> stack = new Stack<int>();
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            /*
+             * 1. loop element from num2 into stack
+             * 2. When found any number > stack.Peek(), means it will be the ans for the number in nums1
+            */
+            foreach (var num in nums2)
+            {
+                while (stack.Count > 0 && stack.Peek() < num)
+                    dict.Add(stack.Pop(), num);
+                stack.Push(num);
+            }
+
+            for (int i = 0; i < nums1.Length; i++)
+                nums1[i] = dict.GetValueOrDefault(nums1[i], -1);
+
+            return nums1;
+        }
+    }
+}
