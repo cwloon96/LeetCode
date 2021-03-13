67e3cf92427dd33cd37ad47004d5097d8792fdc2
Woei Loon <woeiloon96@gmail.com>
2021-03-14 07:04:37 +0800

    392. Is Subsequence

---
+++ LeetCode.Test/Q301-400/Q392Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q301_400;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q301_400
+{
+    class Q392Test
+    {
+        [Test]
+        public void Test()
+        {
+            bool result = new Q392().IsSubsequence("abc", "ahbgdc");
+
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ LeetCode/Q301-400/Q392.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Q301_400
+{
+    public class Q392
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            int i = 0;
+            for (int j = 0; j < t.Length && i < s.Length; j++)
+                if (t[j] == s[i]) i++;
+
+            return i == s.Length;
+        }
+    }
+}
