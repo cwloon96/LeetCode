9e2f228a732dddf80a71bda731338b1d48a16355
Woei Loon <woeiloon96@gmail.com>
2021-03-15 06:31:19 +0800

    1496. Path Crossing

---
+++ LeetCode.Test/Q1401-1500/Q1496Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q1401_1500;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1401_1500
+{
+    class Q1496Test
+    {
+        [Test]
+        public void Test()
+        {
+            bool result = new Q1496().IsPathCrossing("NES");
+
+            Assert.AreEqual(false, result);
+        }
+    }
+}
---
+++ LeetCode/Q1401-1500/Q1496.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Q1401_1500
+{
+    public class Q1496
+    {
+        public bool IsPathCrossing(string path)
+        {
+            List<int[]> list = new List<int[]>();
+            int[] origin = new[] { 0, 0 };
+
+            list.Add(new[] { 0, 0 });
+            foreach (var character in path)
+            {
+                int[] movement = null;
+
+                if (character == 'N')
+                    movement = new[] { 0, 1 };
+                else if (character == 'E')
+                    movement = new[] { 1, 0 };
+                else if (character == 'S')
+                    movement = new[] { 0, -1 };
+                else if (character == 'W')
+                    movement = new[] { -1, 0 };
+
+                origin[0] += movement[0];
+                origin[1] += movement[1];
+
+                if (list.Any(x => x.SequenceEqual(origin)))
+                    return true;
+
+                list.Add(new[] { origin[0], origin[1] });
+            }
+
+            return false;
+        }
+    }
+}
