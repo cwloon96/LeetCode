80ed1ec6afb47df8260ef862e78af1b3ff210d6e
Woei Loon <woeiloon96@gmail.com>
2021-03-16 07:11:42 +0800

    167. Two Sum II - Input array is sorted

---
+++ LeetCode.Test/Q101-200/Q167Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q101_200;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q101_200
+{
+    class Q167Test
+    {
+        [Test]
+        public void Test()
+        {
+            int[] result = new Q167().TwoSum(new int[] { -1, 0 }, -1);
+            CollectionAssert.AreEqual(new[] { 1, 2 }, result);
+        }
+    }
+}
---
+++ LeetCode/Q101-200/Q167.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Q101_200
+{
+    public class Q167
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            int[] result = new int[2];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int remain = target - numbers[i];
+                if (dict.ContainsKey(remain))
+                {
+                    result[0] = dict[remain];
+                    result[1] = i + 1;
+                    break;
+                }
+
+                if (!dict.ContainsKey(numbers[i]))
+                    dict.Add(numbers[i], i + 1);
+            }
+
+            return result;
+        }
+    }
+}
