f7246b1bb2fcf04ba99c800eb58ae814d1da7c79
Woei Loon <woeiloon96@gmail.com>
2021-03-17 06:42:04 +0800

    122. Best Time to Buy and Sell Stock II

---
+++ LeetCode.Test/Q101-200/Q122Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q101_200;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q101_200
+{
+    class Q122Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q122().MaxProfit(new int[] { 1, 2, 3, 4, 5 });
+            Assert.AreEqual(4, result);
+        }
+    }
+}
---
+++ LeetCode/Q101-200/Q122.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Q101_200
+{
+    public class Q122
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int result = 0;
+
+            for (int i = 0; i < prices.Length - 1; i++)
+                if (prices[i] < prices[i + 1])
+                    result += prices[i + 1] - prices[i];
+
+            return result;
+        }
+    }
+}
