f6cb2a2e39ccda73fbe4414326e9b1e8ef76ade5
Woei Loon <woeiloon96@gmail.com>
2021-03-18 06:40:26 +0800

    697. Degree of an Array

---
+++ LeetCode.Test/Q601-700/Q697Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q601_700;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q601_700
+{
+    class Q697Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q697().FindShortestSubArray(new[] { 2, 1, 1, 2, 1, 3, 3, 3, 1, 3, 1, 3, 2 });
+            Assert.AreEqual(7, result);
+        }
+    }
+}
---
+++ LeetCode/Q601-700/Q697.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Q601_700
+{
+    public class Q697
+    {
+        public int FindShortestSubArray(int[] nums)
+        {
+            Dictionary<int, int> count = new Dictionary<int, int>(); // occurance of each element
+            Dictionary<int, int> first = new Dictionary<int, int>(); // index of first element of each numbers
+            int result = 0, max = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (count.ContainsKey(nums[i]))
+                    count[nums[i]]++;
+                else
+                {
+                    count[nums[i]] = 1;
+                    first[nums[i]] = i;
+                }
+
+                if (count[nums[i]] > max)
+                {
+                    max = count[nums[i]];
+                    // last - first + 1 = array size
+                    result = i - first[nums[i]] + 1;
+                }
+                else if (count[nums[i]] == max)
+                    result = Math.Min(result, i - first[nums[i]] + 1);
+            }
+
+            return result;
+        }
+    }
+}
