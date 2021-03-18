defc4e099cd150ceec70f1ab472c8f0901569063
Woei Loon <woeiloon96@gmail.com>
2021-03-19 07:24:59 +0800

    183. Customers Who Never Order (sql)

---
+++ LeetCode/Q101-200/Q183.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Q101_200
+{
+    public class Q183
+    {
+        public void Sql()
+        {
+            string sql = @"SELECT Name AS Customers FROM Customers a
+                           WHERE NOT EXISTS (
+                               SELECT * FROM Orders b WHERE a.Id = b.CustomerId
+                           );";
+        }
+    }
+}
