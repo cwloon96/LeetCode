ecbd46e76388e679ff2784750bf7de28f2ceab24
Woei Loon <woeiloon96@gmail.com>
2021-03-20 12:52:38 +0800

    292. Nim Game

---
+++ LeetCode.Test/Q201-300/Q292Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q201_300;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q201_300
+{
+    class Q292Test
+    {
+        [Test]
+        public void Test()
+        {
+            bool result = new Q292().CanWinNim(4);
+            Assert.AreEqual(false, result);
+        }
+    }
+}
---
+++ LeetCode/Q201-300/Q292.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Q201_300
+{
+    public class Q292
+    {
+        public bool CanWinNim(int n)
+        {
+            /* when number is 4K, first person sure lose
+             * Eg: 4
+             * No matter first person pick 1 ~ 3, the second person can pick the remaining and win
+             * Eg: 8
+             * No matter first person pick 1 ~ 3, the second person can pick until the remaining = 4
+             */
+            return n % 4 != 0;
+        }
+    }
+}
