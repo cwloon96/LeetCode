880f845c87ec11c2dace9cb88e976fd3bc9b4a8f
Woei Loon <woeiloon96@gmail.com>
2021-03-22 07:01:12 +0800

    888. Fair Candy Swap

---
+++ LeetCode.Test/Q801-900/Q888Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q801_900;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q801_900
+{
+    class Q888Test
+    {
+        [Test]
+        public void Test()
+        {
+            int[] result = new Q888().FairCandySwap(new[] { 1, 1 }, new[] { 2, 2 });
+            CollectionAssert.AreEqual(new[] { 1, 2 }, result);
+        }
+    }
+}
---
+++ LeetCode/Q801-900/Q888.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Q801_900
+{
+    public class Q888
+    {
+        public int[] FairCandySwap(int[] A, int[] B)
+        {
+            int alice = 0;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (int i in A)
+            {
+                alice += i;
+                if (dict.ContainsKey(i))
+                    dict[i]++;
+                else
+                    dict[i] = 1;
+            }
+
+            int bob = 0;
+            foreach (int i in B)
+                bob += i;
+
+            int avg = (alice + bob) / 2;
+            int[] result = new int[2];
+
+            foreach (int i in B)
+            {
+                // how much still needed from A after dudecting i
+                int left = avg - (bob - i);
+                if (dict.ContainsKey(left) && (alice - left + i == avg))
+                {
+                    result[0] = left;
+                    result[1] = i;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
