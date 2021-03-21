88bbf49068b42a979e0e8f7864143b40ab0280ff
Woei Loon <woeiloon96@gmail.com>
2021-03-21 08:13:26 +0800

    1556. Thousand Separator

---
+++ LeetCode.Test/Q1501-1600/Q1556Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q1501_1600;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1501_1600
+{
+    class Q1556Test
+    {
+        [Test]
+        public void Test()
+        {
+            string result = new Q1556().ThousandSeparator(1234);
+            Assert.AreEqual("1.234", result);
+        }
+    }
+}
---
+++ LeetCode/Q1501-1600/Q1556.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace LeetCode.Q1501_1600
+{
+    public class Q1556
+    {
+        public string ThousandSeparator(int n)
+        {
+            if (n < 1000)
+                return n.ToString();
+
+            StringBuilder sb = new StringBuilder();
+
+            int digit = 0;
+            while (n > 0)
+            {
+                sb.Insert(0, n % 10);
+
+                digit++;
+                // n >= 10 to check if n is first digit, if yes then not going to add dot
+                if (digit % 3 == 0 && n >= 10)
+                    sb.Insert(0, '.');
+
+                n /= 10;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
