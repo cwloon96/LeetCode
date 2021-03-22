345c09742bcdc83fb38ecd5b9ff12e9ce7a47203
Woei Loon <woeiloon96@gmail.com>
2021-03-23 06:37:29 +0800

    1624. Largest Substring Between Two Equal Characters

---
+++ LeetCode.Test/Q1601-1700/Q1624Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q1601_1700;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1601_1700
+{
+    class Q1624Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q1624().MaxLengthBetweenEqualCharacters("abca");
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ LeetCode/Q1601-1700/Q1624.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Q1601_1700
+{
+    public class Q1624
+    {
+        public int MaxLengthBetweenEqualCharacters(string s)
+        {
+            int result = -1;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dict.ContainsKey(s[i]))
+                {
+                    int length = i - dict[s[i]] - 1;
+                    result = Math.Max(result, length);
+                }
+                else
+                {
+                    dict[s[i]] = i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
