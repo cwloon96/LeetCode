536dd0cee28acb61d5520857e45c50f371f06537
Woei Loon <woeiloon96@gmail.com>
2021-03-24 06:28:56 +0800

    389. Find the Difference

---
+++ LeetCode.Test/Q301-400/Q389Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q301_400;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q301_400
+{
+    class Q389Test
+    {
+        [Test]
+        public void Test()
+        {
+            char result = new Q389().FindTheDifference("abc", "abcd");
+
+            Assert.AreEqual('d', result);
+        }
+    }
+}
---
+++ LeetCode/Q301-400/Q389.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Q301_400
+{
+    public class Q389
+    {
+        public char FindTheDifference(string s, string t)
+        {
+            char result = t[t.Length - 1];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                result ^= s[i];
+                result ^= t[i];
+            }
+
+            return result;
+        }
+    }
+}
