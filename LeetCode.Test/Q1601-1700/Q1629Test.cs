f99bf3a4e57d6f5bc762bb3db5197a8fd6804993
Woei Loon <woeiloon96@gmail.com>
2021-03-25 06:37:12 +0800

    1629. Slowest Key

---
+++ LeetCode.Test/Q1601-1700/Q1629Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q1601_1700;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1601_1700
+{
+    class Q1629Test
+    {
+        [Test]
+        public void Test()
+        {
+            char result = new Q1629().SlowestKey(new[] { 9, 29, 49, 50 }, "cbcd");
+
+            Assert.AreEqual('c', result);
+        }
+    }
+}
---
+++ LeetCode/Q1601-1700/Q1629.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Q1601_1700
+{
+    public class Q1629
+    {
+        public char SlowestKey(int[] releaseTimes, string keysPressed)
+        {
+            char result = keysPressed[0];
+            int max = releaseTimes[0];
+
+            for (int i = 1; i < releaseTimes.Length; i++)
+            {
+                int duration = releaseTimes[i] - releaseTimes[i - 1];
+
+                if (duration > max)
+                {
+                    max = duration;
+                    result = keysPressed[i];
+                }
+                else if (max == duration)
+                    result = result > keysPressed[i] ? result : keysPressed[i];
+            }
+
+            return result;
+        }
+    }
+}
