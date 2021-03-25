1a48fc5b8d54419e3eeb387afa315ce0b91e3470
Woei Loon <woeiloon96@gmail.com>
2021-03-26 07:03:55 +0800

    1544. Make The String Great

---
+++ LeetCode.Test/Q1501-1600/Q1544Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q1501_1600;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1501_1600
+{
+    class Q1544Test
+    {
+        [Test]
+        public void Test()
+        {
+            string result = new Q1544().MakeGood("abBAcC");
+            Assert.AreEqual("", result);
+        }
+    }
+}
---
+++ LeetCode/Q1501-1600/Q1544.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Q1501_1600
+{
+    public class Q1544
+    {
+        public string MakeGood(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var c in s)
+            {
+                if (stack.Count == 0)
+                    stack.Push(c);
+                else
+                {
+                    if (Math.Abs(stack.Peek() - c) == 32)
+                        stack.Pop();
+                    else
+                        stack.Push(c);
+                }
+            }
+
+            StringBuilder result = new StringBuilder();
+            while (stack.Count > 0)
+                result.Insert(0, stack.Pop());
+
+            return result.ToString();
+        }
+    }
+}
