332794c287a3509e387d4c45e1495191b8a280e7
Woei Loon <woeiloon96@gmail.com>
2021-03-27 07:08:18 +0800

    696. Count Binary Substrings

---
+++ LeetCode.Test/Q601-700/Q696Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q601_700;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q601_700
+{
+    class Q696Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q696().CountBinarySubstrings("00110011");
+            Assert.AreEqual(6, result);
+        }
+    }
+}
---
+++ LeetCode/Q601-700/Q696.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeetCode.Q601_700
+{
+    public class Q696
+    {
+        public int CountBinarySubstrings(string s)
+        {
+            int previous = 0, current = 1, result = 0;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s[i - 1])
+                    current++;
+                else
+                {
+                    // if previous = 3 and current = 2 -> 00011 = 2 valid substring [01, 0011]
+                    result += Math.Min(previous, current);
+                    previous = current;
+                    current = 1;
+                }
+            }
+
+            return result + Math.Min(previous, current);
+        }
+    }
+}
