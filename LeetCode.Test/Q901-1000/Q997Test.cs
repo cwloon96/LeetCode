86af79074a4098a1d6300e9c76c8bc0c527e2404
Woei Loon <woeiloon96@gmail.com>
2021-03-28 08:28:23 +0800

    997. Find the Town Judge

---
+++ LeetCode.Test/Q901-1000/Q997Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q901_1000;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q901_1000
+{
+    class Q997Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q997().FindJudge(2, new int[][] { new int[] { 1, 2 } });
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ LeetCode/Q901-1000/Q997.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Q901_1000
+{
+    public class Q997
+    {
+        public int FindJudge(int N, int[][] trust)
+        {
+            int[] trusted = new int[N + 1];
+
+            for (int i = 0; i < trust.Length; i++)
+            {
+                trusted[trust[i][0]]--;
+                trusted[trust[i][1]]++;
+            }
+
+            // Since everyone will trust the judge except himself = N - 1
+            for (int i = 1; i < trusted.Length; i++)
+                if (trusted[i] == N - 1)
+                    return i;
+
+            return -1;
+        }
+    }
+}
