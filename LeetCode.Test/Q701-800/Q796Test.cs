d22f25f091e331f5312b47aaa720e27d793dd3db
Woei Loon <woeiloon96@gmail.com>
2021-03-29 06:57:07 +0800

    796. Rotate String

---
+++ LeetCode.Test/Q701-800/Q796Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q701_800;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q701_800
+{
+    public class Q796Test
+    {
+        [Test]
+        public void Test()
+        {
+            bool result = new Q796().RotateString("abcde", "cdeab");
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ LeetCode/Q701-800/Q796.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Q701_800
+{
+    public class Q796
+    {
+        public bool RotateString(string A, string B)
+        {
+            if (A.Length != B.Length)
+                return false;
+
+            if (A.Length == 0)
+                return true;
+
+            // store possible shifts
+            List<int> shifts = new List<int>();
+            for (int i = 0; i < A.Length; i++)
+                if (A[i] == B[0])
+                    shifts.Add(i);
+
+            foreach (var shift in shifts)
+            {
+                int i = 0;
+                for (; i < A.Length; i++)
+                {
+                    // calculate shifted position
+                    int shifted = i - shift >= 0 ? i - shift : A.Length - shift + i;
+
+                    if (A[i] != B[shifted])
+                        break;
+                }
+
+                if (i == A.Length)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
