ca7ba069b7956a5bcda9024237fde149bfa30afb
Woei Loon <woeiloon96@gmail.com>
2021-04-01 06:40:43 +0800

    345. Reverse Vowels of a String

---
+++ LeetCode.Test/Q301-400/Q345Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q301_400;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q301_400
+{
+    class Q345Test
+    {
+        [Test]
+        public void Test()
+        {
+            string result = new Q345().ReverseVowels("aA");
+
+            Assert.AreEqual("Aa", result);
+        }
+    }
+}
---
+++ LeetCode/Q301-400/Q345.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Q301_400
+{
+    public class Q345
+    {
+        public string ReverseVowels(string s)
+        {
+            List<char> vowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+            int left = 0;
+            int right = s.Length - 1;
+            char[] arr = s.ToCharArray();
+            while (left < right)
+            {
+                while (left < right && !vowels.Contains(char.ToLower(s[left])))
+                    left++;
+
+                while (left < right && !vowels.Contains(char.ToLower(s[right])))
+                    right--;
+
+                if (left < right)
+                {
+                    var temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+                }
+                left++;
+                right--;
+            }
+
+            return new string(arr);
+        }
+    }
+}
