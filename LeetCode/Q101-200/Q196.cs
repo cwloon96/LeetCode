2d1e9f4f64c2d0a6a588190c67db29692cdc109c
Woei Loon <woeiloon96@gmail.com>
2021-04-02 06:31:00 +0800

    196. Delete Duplicate Emails (sql)

---
+++ LeetCode/Q101-200/Q196.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Q101_200
+{
+    public class Q196
+    {
+        public void Sql()
+        {
+            string sql = @"DELETE a FROM Person a
+                           INNER JOIN Person b
+                           WHERE
+                              a.Id > b.Id AND
+                              a.Email = b.Email;";
+        }
+    }
+}
