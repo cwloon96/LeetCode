e26729bd047e20209298b70b9553e518132ed391
Woei Loon <woeiloon96@gmail.com>
2021-04-03 06:56:55 +0800

    819. Most Common Word

---
+++ LeetCode.Test/Q801-900/Q819Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q801_900;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q801_900
+{
+    class Q819Test
+    {
+        [Test]
+        public void Test()
+        {
+            string result = new Q819().MostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", new[] { "hit" });
+            Assert.AreEqual("ball", result);
+        }
+    }
+}
---
+++ LeetCode/Q801-900/Q819.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Q801_900
+{
+    public class Q819
+    {
+        public string MostCommonWord(string paragraph, string[] banned)
+        {
+            Dictionary<string, int> ban = new Dictionary<string, int>();
+            Dictionary<string, int> count = new Dictionary<string, int>();
+
+            foreach (var word in banned)
+                ban.Add(word, 1);
+
+            char[] separators = new char[] { ' ', ';', ',', '.', '!', '?', '\'' };
+            string[] words = paragraph.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in words)
+            {
+                string lower = word.ToLower();
+                if (!ban.ContainsKey(lower))
+                {
+                    if (count.ContainsKey(lower))
+                        count[lower]++;
+                    else
+                        count[lower] = 1;
+                }
+            }
+
+            return count.First(x => x.Value == count.Values.Max()).Key;
+        }
+    }
+}
