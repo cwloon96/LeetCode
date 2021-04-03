32420443eec38cc969b46081c70f233bbd7dd9a4
Woei Loon <woeiloon96@gmail.com>
2021-04-04 06:57:16 +0800

    9. Palindrome Number

---
+++ LeetCode.Test/Q1-100/Q9Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q1_100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1_100
+{
+    class Q9Test
+    {
+        [Test]
+        public void Test()
+        {
+            var result = new Q9().IsPalindrome(121);
+
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ LeetCode/Q1-100/Q9.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Q1_100
+{
+    public class Q9
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0 || (x != 0 && x % 10 == 0))
+                return false;
+
+            int sum = 0;
+            while (x > sum)
+            {
+                sum = sum * 10 + x % 10;
+                x /= 10;
+            }
+
+            /* sum == x happens when x has even digit
+             * Eg: 1221
+             * x = 12, sum = 12
+             * 
+             * sum / 10 == x happens when x has odd digit
+             * Eg: 121
+             * x = 1, sum = 12
+            */
+            return (sum == x) || (sum / 10 == x);
+        }
+    }
+}
