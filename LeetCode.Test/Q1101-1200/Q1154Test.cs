ef4f80701936104023b8dccd1b393fae60c9da70
Woei Loon <woeiloon96@gmail.com>
2021-04-05 06:26:55 +0800

    1154. Day of the Year

---
+++ LeetCode.Test/Q1101-1200/Q1154Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q1101_1200;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1101_1200
+{
+    class Q1154Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q1154().DayOfYear("2004-03-01");
+            Assert.AreEqual(61, result);
+        }
+    }
+}
---
+++ LeetCode/Q1101-1200/Q1154.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace LeetCode.Q1101_1200
+{
+    public class Q1154
+    {
+        public int DayOfYear(string date)
+        {
+            int[] dates = date.Split('-').Select(int.Parse).ToArray();
+            int[] days = new[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            if (dates[0] % 4 == 0 && (dates[0] % 100 != 0 || dates[0] % 400 == 0))
+                days[1] = 29;
+
+            int day = 0;
+            for (int i = 0; i < dates[1] - 1; i++)
+                day += days[i];
+
+            return day + dates[2];
+        }
+    }
+}
