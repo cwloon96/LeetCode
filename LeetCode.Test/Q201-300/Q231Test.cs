d230674f60de9e1e2abea7330728a53cce0bb5b9
Woei Loon <woeiloon96@gmail.com>
2021-04-07 06:46:29 +0800

    231. Power of Two

---
+++ LeetCode.Test/Q201-300/Q231Test.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Q201_300;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q201_300
+{
+    class Q231Test
+    {
+        [Test]
+        public void TestBit()
+        {
+            bool result = new Q231().IsPowerOfTwo_Bit(4);
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void TestLoop()
+        {
+            bool result = new Q231().IsPowerOfTwo_Loop(4);
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ LeetCode/Q201-300/Q231.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Q201_300
+{
+    public class Q231
+    {
+        public bool IsPowerOfTwo_Bit(int n)
+        {
+            /* All power 2 = got only 1 bit = 1
+             * 2 = 0000 0010, 1 = 0000 0001 
+             * 4 = 0000 0100, 3 = 0000 0011
+             * thus n & (n - 1) will get 0000 0000
+            */
+            return n > 0 && (n & (n - 1)) == 0;
+        }
+
+        public bool IsPowerOfTwo_Loop(int n)
+        {
+            if (n <= 0) return false;
+
+            while (n % 2 == 0)
+                n /= 2;
+            
+            return n == 1;
+        }
+    }
+}
