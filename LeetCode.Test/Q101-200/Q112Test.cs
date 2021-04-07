fc5b5d7cb956599b66d9da0ffa57f4155efe315f
Woei Loon <woeiloon96@gmail.com>
2021-04-08 07:05:47 +0800

    112. Path Sum

---
+++ LeetCode.Test/Q101-200/Q112Test.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Common.Models;
+using LeetCode.Q101_200;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q101_200
+{
+    class Q112Test
+    {
+        [Test]
+        public void Test()
+        {
+            TreeNode root = new TreeNode(1, new TreeNode(2));
+            bool result = new Q112().HasPathSum(root, 1);
+
+            Assert.AreEqual(false, result);
+        }
+    }
+}
---
+++ LeetCode/Q101-200/Q112.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Common.Models;
+
+namespace LeetCode.Q101_200
+{
+    public class Q112
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null)
+                return false;
+
+            if (root.left == null && root.right == null)
+                return targetSum - root.val == 0;
+
+            return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+        }
+    }
+}
