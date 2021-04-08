7990bf42c7e1d39403be2ed8ac1b26fcffb57243
Woei Loon <woeiloon96@gmail.com>
2021-04-09 06:37:51 +0800

    197. Rising Temperature

---
+++ LeetCode/Q101-200/Q197.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode.Q101_200
+{
+    class Q197
+    {
+        public void Sql()
+        {
+            string sql = @"SELECT a.id 
+                            FROM Weather a
+                            INNER JOIN Weather b ON DATEDIFF(a.recordDate, b.recordDate) = 1 AND a.Temperature > b.Temperature";
+        }
+    }
+}
