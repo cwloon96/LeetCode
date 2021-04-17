10d47d9a95dd9404e850843c2c87ac643a0e194a
Woei Loon <woeiloon96@gmail.com>
2021-04-17 08:04:52 +0800

    revision Q703

--- LeetCode/Q701-800/Q703.cs
+++ LeetCode/Q701-800/Q703.cs
@@ -24,6 +24,11 @@ namespace LeetCode.Q701_800
                 dict.Add(val, 1);
             size++;
 
+            /* Looking for k largest value -> only need to store k size value and the smallest value will be the ans
+             * EG: k = 3, nums = [1, 2, 3, 4]
+             * when adding 3, dict will sorted as 1 -> 2 -> 3, thus k(3) largest = 1
+             * when adding 4, dict will became 1 -> 2 -> 3 -> 4, it exceed k size so we remove the first value and k largest = 2
+             */
             if (size > target)
             {
                 var first = dict.First();
