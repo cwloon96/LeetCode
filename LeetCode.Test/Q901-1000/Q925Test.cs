62bb2a6d2de0714118003f1d7fe957761eefddd0
Woei Loon <woeiloon96@gmail.com>
2021-08-22 09:33:37 +0800

    925. Long Pressed Name

---
+++ LeetCode.Test/Q901-1000/Q925Test.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Q901_1000;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q901_1000
+{
+    class Q925Test
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(true, new Q925().IsLongPressedName("alex", "aaleex"));
+        }
+
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(false, new Q925().IsLongPressedName("alex", "aaleexa"));
+        }
+
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(false, new Q925().IsLongPressedName("saeed", "ssaaedd"));
+        }
+    }
+}
---
+++ LeetCode/Q901-1000/Q925.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Q901_1000
+{
+    public class Q925
+    {
+        public bool IsLongPressedName(string name, string typed)
+        {
+            if (typed.Length < name.Length)
+                return false;
+
+            int i = 0;
+
+            for (int j = 0; j < typed.Length; j++)
+            {
+                if (i < name.Length && name[i] == typed[j])
+                    i++;
+                else
+                {
+                    if (j == 0 || typed[j - 1] != typed[j])
+                        return false;
+                }
+            }
+
+            return i == name.Length;
+        }
+    }
+}
