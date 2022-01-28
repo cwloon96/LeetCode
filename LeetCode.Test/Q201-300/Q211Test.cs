486343e5da41d8ff2471a2b5d8d19bc717d85e27
Woei Loon <woeiloon96@gmail.com>
2022-01-28 11:24:41 +0800

    211. Design Add and Search Words Data Structure

---
+++ LeetCode.Test/Q201-300/Q211Test.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Q201_300;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q201_300
+{
+    public class Q211Test
+    {
+        [Test]
+        public void Test()
+        {
+            var dict = new WordDictionary();
+            dict.AddWord("bad");
+            dict.AddWord("dad");
+            dict.AddWord("mad");
+
+            Assert.AreEqual(false, dict.Search("pad"));
+            Assert.AreEqual(true, dict.Search("bad"));
+            Assert.AreEqual(true, dict.Search(".ad"));
+            Assert.AreEqual(true, dict.Search("b.."));
+        }
+    }
+}
---
+++ LeetCode/Q201-300/Q211.cs
@@ -0,0 +1,67 @@
+﻿namespace LeetCode.Q201_300
+{
+    public class WordDictionary
+    {
+        class TrieNode
+        {
+            public TrieNode[] children { get; set; }
+            public bool End { get; set; }
+
+            public TrieNode()
+            {
+                children = new TrieNode[26];
+            }
+        }
+
+        private TrieNode root;
+
+        public WordDictionary()
+        {
+            root = new TrieNode();
+        }
+
+        public void AddWord(string word)
+        {
+            var temp = root;
+
+            foreach (var c in word)
+            {
+                if (temp.children[c - 'a'] == null)
+                    temp.children[c - 'a'] = new TrieNode();
+
+                temp = temp.children[c - 'a'];
+            }
+
+            temp.End = true;
+        }
+
+        public bool Search(string word)
+        {
+            return Search(word, root, 0);
+        }
+
+        private bool Search(string word, TrieNode node, int index)
+        {
+            if (node == null)
+                return false;
+
+            if (index == word.Length)
+                return node.End;
+
+            var character = word[index];
+
+            if (character == '.')
+            {
+                for(int i = 0; i < node.children.Length; i++)
+                {
+                    if (node.children[i] != null && Search(word, node.children[i], index + 1))
+                        return true;
+                }
+
+                return false;
+            }
+
+            return Search(word, node.children[character - 'a'], index + 1);
+        }
+    }
+}
