ca4ba7fd0ae87a9d9ff3a86fa9c1bdf5aead1822
Woei Loon <woeiloon96@gmail.com>
2022-02-15 08:26:02 +0800

    Q179. Largest Number

---
+++ LeetCode.Test/Q101-200/Q179Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q101_200;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q101_200
+{
+    class Q179Test
+    {
+        [Test]
+        public void Test()
+        {
+            string result = new Q179().LargestNumber(new int[] { 3, 30, 34, 5, 9 });
+            Assert.AreEqual("9534330", result);
+        }
+    }
+}
---
+++ LeetCode/Q101-200/Q179.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode.Q101_200
+{
+    public class Q179
+    {
+        public string LargestNumber(int[] nums)
+        {
+            var arr = new string[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+                arr[i] = nums[i].ToString();
+
+            Array.Sort(arr, (s1, s2) => {
+                var case1 = s1 + s2;
+                var case2 = s2 + s1;
+
+                return case2.CompareTo(case1);
+            });
+
+            if (arr[0][0] == '0')
+                return "0";
+
+            var sb = new StringBuilder();
+            for (int i = 0; i < arr.Length; i++)
+                sb.Append(arr[i]);
+
+            return sb.ToString();
+        }
+    }
+}
