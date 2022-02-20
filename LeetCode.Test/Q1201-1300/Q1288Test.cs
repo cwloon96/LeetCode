f2025fdca10f101d3cc4f1cc86af8bec03f160af
Woei Loon <woeiloon96@gmail.com>
2022-02-20 19:25:53 +0800

    1288. Remove Covered Intervals

---
+++ LeetCode.Test/Q1201-1300/Q1288Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q1201_1300;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1201_1300
+{
+    public class Q1288Test
+    {
+        [Test]
+        public void Test()
+        {
+            var result = new Q1288().RemoveCoveredIntervals(new[] { new[] { 1, 4 }, new[] { 3, 6 }, new[] { 2, 8 }, new[] { 2, 4 } });
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ LeetCode/Q1201-1300/Q1288.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode.Q1201_1300
+{
+    public class Q1288
+    {
+        public int RemoveCoveredIntervals(int[][] intervals)
+        {
+            // Sort left asc, right desc
+            // Eg:  [[1,4],[3,6],[2,8],[2,4]] ->  [[1,4],[2,8],[2,4],[3,6]]
+            Array.Sort(intervals, (x, y) => {
+                if (x[0] == y[0])
+                    return y[1].CompareTo(x[1]);
+
+                return x[0].CompareTo(y[0]);
+            });
+
+            int count = 0, maxRight = 0;
+
+            // each time we found a larger right means new intervals
+            foreach (var interval in intervals)
+            {
+                if (interval[1] > maxRight)
+                {
+                    count++;
+                    maxRight = interval[1];
+                }
+            }
+
+            return count;
+        }
+    }
+}
