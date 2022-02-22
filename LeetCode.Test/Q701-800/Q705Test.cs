d3714f633f7b3c11a522190355e50ef3755c26b3
Woei Loon <woeiloon96@gmail.com>
2022-02-22 18:44:32 +0800

    705. Design HashSet

---
+++ LeetCode.Test/Q701-800/Q705Test.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Q701_800;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q701_800
+{
+    class Q705Test
+    {
+        [Test]
+        public void Test()
+        {
+            var set = new MyHashSet();
+            set.Add(1);
+            set.Add(31);
+            set.Add(81);
+            set.Add(1011);
+
+            Assert.AreEqual(true, set.Contains(1));
+            Assert.AreEqual(true, set.Contains(31));
+            Assert.AreEqual(true, set.Contains(81));
+            Assert.AreEqual(true, set.Contains(1011));
+
+            Assert.AreEqual(false, set.Contains(2));
+
+            set.Remove(1);
+            Assert.AreEqual(false, set.Contains(1));
+        }
+    }
+}
---
+++ LeetCode/Q701-800/Q705.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Q701_800
+{
+    public class MyHashSet
+    {
+        private readonly int[] _nums;
+
+        public MyHashSet()
+        {
+            // max value is 1000000
+            // we will store 32 value in each index
+            // eg: 0 = 0 - 31, 1 = 32 - 63
+            // 1000000 / 32 = 31250
+            _nums = new int[31251];
+        }
+
+        public void Add(int key)
+        {
+            _nums[GetIndex(key)] |= GetMask(key);
+        }
+
+        public void Remove(int key)
+        {
+            _nums[GetIndex(key)] &= ~GetMask(key);
+        }
+
+        public bool Contains(int key)
+        {
+           return (_nums[GetIndex(key)] & GetMask(key)) != 0;
+        }
+
+        private int GetIndex(int key) => key / 32;
+
+        private int GetMask(int key)
+        {
+            key %= 32;
+            return (1 << key);
+        }
+    }
+}
