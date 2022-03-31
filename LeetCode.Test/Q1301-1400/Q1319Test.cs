265cb410bb9bd1383efd6e4458248ccd9e4c6567
Woei Loon <woeiloon96@gmail.com>
2022-04-01 07:59:26 +0800

    1319. Number of Operations to Make Network Connected

---
+++ LeetCode.Test/Q1301-1400/Q1319Test.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Q1301_1400;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1301_1400
+{
+    class Q1319Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q1319().MakeConnected(5, new[] { 
+                new[] { 0, 1 },
+                new[] { 0, 2 },
+                new[] { 1, 2 },
+                new[] { 2, 3 },
+            });
+
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ LeetCode/Q1301-1400/Q1319.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Q1301_1400
+{
+    public class Q1319
+    {
+        public int MakeConnected(int n, int[][] connections)
+        {
+            if (connections.Length < n - 1) return -1; // To connect all nodes need at least n-1 edges
+            int[] parent = new int[n];
+            for (int i = 0; i < n; i++) parent[i] = i;
+            int components = n;
+            foreach (int[] c in connections)
+            {
+                int p1 = FindParent(parent, c[0]);
+                int p2 = FindParent(parent, c[1]);
+                if (p1 != p2)
+                {
+                    parent[p1] = p2; // Union 2 component
+                    components--;
+                }
+            }
+
+            int FindParent(int[] parent, int i) {
+                //while (i != parent[i]) i = parent[i];
+                //return i; // Without Path Compression
+
+                if (i == parent[i]) return i;
+                return parent[i] = FindParent(parent, parent[i]);
+            };
+
+            return components - 1; // Need (components-1) cables to connect components together
+        }
+    }
+}
