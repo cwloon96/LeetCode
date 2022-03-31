e0e31c336f2d86dbffa37673959c297ae640bebb
Woei Loon <woeiloon96@gmail.com>
2022-04-01 07:58:47 +0800

    1029. Two City Scheduling

---
+++ LeetCode.Test/Q1001-1100/Q1029Test.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Q1001_1100;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1001_1100
+{
+    class Q1029Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q1029().TwoCitySchedCost(new[] {
+                new[] { 10, 20 },
+                new[] { 30, 200 },
+                new[] { 400, 50 },
+                new[] { 30, 20 },
+            });
+            Assert.AreEqual(110, result);
+        }
+    }
+}
---
+++ LeetCode/Q1001-1100/Q1029.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.Q1001_1100
+{
+    public class Q1029
+    {
+        public int TwoCitySchedCost(int[][] costs)
+        {
+            Array.Sort(costs, (a, b) => {
+                if (a[0] - a[1] < b[0] - b[1])
+                    return -1;
+                return 1;
+            });
+
+            int result = 0;
+            int half = costs.Length / 2;
+
+            for (int i = 0; i < half; i++)
+                result += costs[i][0] + costs[i + half][1];
+
+            return result;
+        }
+    }
+}
