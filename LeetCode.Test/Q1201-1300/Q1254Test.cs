dab9262235edabbd88499e1ef765044196879265
Woei Loon <woeiloon96@gmail.com>
2022-04-01 07:59:02 +0800

    1254. Number of Closed Islands

---
+++ LeetCode.Test/Q1201-1300/Q1254Test.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Q1201_1300;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1201_1300
+{
+    public class Q1254Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q1254().ClosedIsland(new int[][] {
+                new int[] { 1, 1, 1, 1, 1, 1, 1, 0 },
+                new int[] { 1, 0, 0, 0, 0, 1, 1, 0 },
+                new int[] { 1, 0, 1, 0, 1, 1, 1, 0 },
+                new int[] { 1, 0, 0, 0, 0, 1, 0, 1 },
+                new int[] { 1, 1, 1, 1, 1, 1, 1, 0 },
+            });
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ LeetCode/Q1201-1300/Q1254.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode.Q1201_1300
+{
+    public class Q1254
+    {
+        public int ClosedIsland(int[][] grid)
+        {
+            int result = 0;
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == 0)
+                    {
+                        if (IsClosed(grid, i, j))
+                            result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private bool IsClosed(int[][] grid, int row, int col)
+        {
+            if (row < 0 || row >= grid.Length ||
+                col < 0 || col >= grid[row].Length)
+                return false;
+
+            if (grid[row][col] == 1)
+                return true;
+
+            grid[row][col] = 1;
+
+            bool result = true;
+
+            if (!IsClosed(grid, row + 1, col))
+                result = false;
+
+            if (!IsClosed(grid, row - 1, col))
+                result = false;
+
+            if (!IsClosed(grid, row, col + 1))
+                result = false;
+
+            if (!IsClosed(grid, row, col - 1))
+                result = false;
+
+            return result;
+        }
+    }
+}
