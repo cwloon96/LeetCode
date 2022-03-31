8248a453210bf1c03c0cb3e7783e03886b22c81e
Woei Loon <woeiloon96@gmail.com>
2022-04-01 07:59:58 +0800

    799. Champagne Tower

---
+++ LeetCode.Test/Q701-800/Q799Test.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Q701_800;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q701_800
+{
+    class Q799Test
+    {
+        [Test]
+        public void Test()
+        {
+            double result = new Q799().ChampagneTower(6, 3, 1);
+            Assert.AreEqual(0.25, result);
+        }
+    }
+}
---
+++ LeetCode/Q701-800/Q799.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.Q701_800
+{
+    public class Q799
+    {
+        public double ChampagneTower(int poured, int query_row, int query_glass)
+        {
+            var dp = new double[query_row + 1][];
+            for (int i = 0; i < dp.Length; i++)
+                dp[i] = new double[i + 1];
+
+            dp[0][0] = poured;
+
+            for (int i = 0; i < query_row; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (dp[i][j] > 1)
+                    {
+                        dp[i + 1][j] += (dp[i][j] - 1) / 2;
+                        dp[i + 1][j + 1] += (dp[i][j] - 1) / 2;
+                    }
+                }
+            }
+
+            return Math.Min(dp[query_row][query_glass], 1);
+        }
+    }
+}
