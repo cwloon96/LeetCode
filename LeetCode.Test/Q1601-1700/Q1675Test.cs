555981d43ce4943d5c181e02fac0970e5cc86b2f
Woei Loon <woeiloon96@gmail.com>
2022-04-01 07:59:49 +0800

    1675. Minimize Deviation in Array

---
+++ LeetCode.Test/Q1601-1700/Q1675Test.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Q1601_1700;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q1601_1700
+{
+    class Q1675Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q1675().MinimumDeviation(new[] { 1, 2, 3, 4 });
+
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ LeetCode/Q1601-1700/Q1675.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Q1601_1700
+{
+    public class Q1675
+    {
+		public int MinimumDeviation(int[] nums)
+		{
+			SortedSet<int> set = new SortedSet<int>();
+
+			foreach (var num in nums)
+			{
+				if (num % 2 == 0)
+					set.Add(num);
+				else
+					set.Add(num * 2);
+			}
+
+			int result = set.Max - set.Min;
+			while (set.Max % 2 == 0)
+			{
+				int last = set.Max;
+				set.Remove(last);
+				set.Add(last / 2);
+				result = Math.Min(result, set.Max - set.Min);
+			}
+
+			return result;
+		}
+	}
+}
