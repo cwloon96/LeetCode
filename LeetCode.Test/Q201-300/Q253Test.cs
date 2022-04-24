85287c8a9807d6ec4efef92975029d9b29119aa4
cwloon96 <woeiloon96@gmail.com>
2022-04-24 21:01:04 +0800

    253. Meeting Rooms II

---
+++ LeetCode.Test/Q201-300/Q253Test.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Q201_300;
+using NUnit.Framework;
+
+namespace LeetCode.Test.Q201_300
+{
+    class Q253Test
+    {
+        [Test]
+        public void Test()
+        {
+            int result = new Q253().MinMeetingRoom(new int[][] { new[] { 9, 10 }, new[] { 4, 9 }, new[] { 5, 17 } });
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int result = new Q253().MinMeetingRoom(new int[][] { new[] { 9, 10 }, new[] { 4, 9 }, new[] { 3, 4 } });
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ LeetCode/Q201-300/Q253.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace LeetCode.Q201_300
+{
+    /// <summary>
+    /// https://medium.com/nerd-for-tech/meeting-room-amazon-interview-problem-72609de8d776
+    /// </summary>
+    public class Q253
+    {
+        public int MinMeetingRoom(int[][] intervals)
+        {
+            var start = new int[intervals.Length];
+
+            for (int i = 0; i < intervals.Length; i++)
+                start[i] = intervals[i][0];
+
+            var end = new int[intervals.Length];
+
+            for(int i = 0; i < intervals.Length; i++)
+                end[i] = intervals[i][1];
+
+            Array.Sort(start);
+            Array.Sort(end);
+
+            int rooms = 0, curr = 0;
+            int startIndex = 0, endIndex = 0;
+
+            while (startIndex < start.Length)
+            {
+                if (start[startIndex] < end[endIndex])
+                {
+                    startIndex++;
+                    curr++;
+                }    
+                else
+                {
+                    endIndex++;
+                    curr--;
+                }
+
+                rooms = Math.Max(curr, rooms);
+            }
+
+            return rooms;
+        }
+    }
+}
